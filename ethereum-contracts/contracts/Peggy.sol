aae4af8443b04e6d89315b2b5ec800c726c8900d
mossid <torecursedivine@gmail.com>
2018-01-26 18:32:17 +0100

    reduced to mvp

--- ethereum-contracts/contracts/Peggy.sol
+++ ethereum-contracts/contracts/Peggy.sol
@@ -1,11 +1,8 @@
 pragma solidity ^0.4.17;
 
 import "./CosmosERC20.sol";
-import "./Valset.sol";
 
-contract Peggy is Valset {
-
-    Valset public valset;
+contract Peggy {
     mapping (bytes => CosmosERC20) cosmosTokenAddress;
 
     function getCosmosTokenAddress(bytes name) internal constant returns (address) {
@@ -44,75 +41,24 @@ contract Peggy is Valset {
      * @param s           output of ECDSA signature
      */
     event Unlock(address to, uint64 value, address token, bytes indexed chain);
-
-    function unlock(address to, uint64 value, address token, bytes chain, uint16[] idxs, uint8[] v, bytes32[] r, bytes32[] s) external returns (bool) {
-        bytes32 hash = keccak256(byte(1), to, value, chain.length, chain);
-        assert(valset.verify(hash, idxs, v, r, s));
-        if (token == address(0)) {
-            assert(to.send(value));
+    
+    function unlock(
+        address[2] addressArg, 
+        uint64 value, 
+        bytes chain, 
+        uint16[] idxs, 
+        uint8[] v, 
+        bytes32[2][] rs 
+    ) external returns (bool) {
+        bytes32 hash = keccak256(byte(1), addressArg[0], value, chain.length, chain);
+        if (addressArg[1] == address(0)) {
+            assert(addressArg[0].send(value));
         } else {
-            assert(ERC20(token).transfer(to, value));
+            assert(ERC20(addressArg[1]).transfer(addressArg[1], value));
         }
-        Unlock(to, value, token, chain);
+        Unlock(addressArg[0], value, addressArg[1], chain);
         return true;
     }
 
-    event Mint(address to, uint64 value, bytes token, bytes indexed chain);
-
-    /// Mints 1:1 backed credit for atoms/photons. Sends the transaction to the smart contract
-    /*
-     * @param to          bytes representation of destination address
-     * @param value       value of transference
-     * @param token       token address in origin chain (0x0 if Ethereum, Cosmos for other values)
-     * @param chain       bytes respresentation of the destination chain
-     * @param idxs        indexes of each validator
-     * @param v           recovery id.
-     * @param r           output of ECDSA signature.
-     * @param s           output of ECDSA signature.
-     */
-    function mint(address to, uint64 value, bytes token, bytes chain, uint16[] idxs, uint8[] v, bytes32[] r, bytes32[] s) external returns (bool) {
-        require(getCosmosTokenAddress(token) != 0);
-        bytes32 hash = keccak256(byte(2), to, value, token.length, token, chain.length, chain); /*, signatureNonce++*/
-        assert(Valset.verify(hash, idxs, v, r, s));
-        assert(CosmosERC20(getCosmosTokenAddress(token)).mint(to, value));
-        Mint(to, value, token, chain);
-        return true;
-    }
-
-    event Burn(bytes to, uint64 value, bytes token, bytes indexed chain);
-
-    /// burns receiving funds, unlocking funds in the Cosmos Hub
-    /*
-     * @param to          bytes representation of destination address
-     * @param value       value of transfer
-     * @param token       bytes representation of Cosmos token
-     * @param chain       bytes respresentation of the Cosmos chain
-     */
-    function burn(bytes to, uint64 value, bytes token, bytes chain) external returns (bool) {
-        assert(CosmosERC20(getCosmosTokenAddress(token)).burn(msg.sender, value));
-        Burn(to, value, token, chain); /*, witnessNonce++*/
-        return true;
-    }
-
-    event Register(bytes name, address token);
-
-    /// Deploys new CosmosERC20 contract and stores it in a mapping.
-    /// Registers new Cosmos token name with its CosmosERC20 address. Called by the relayers.
-    /*
-     * @param string       bytes representation of destination address
-     * @param token       token address in origin chain (0x0 if Ethereum, Cosmos for other values)
-     * @param idxs        indexes of each validator
-     * @param v           recovery id.
-     * @param r           output of ECDSA signature.
-     * @param s           output of ECDSA signature.
-     */
-    function register(bytes name, address token, uint16[] idxs, uint8[] v, bytes32[] r, bytes32[] s) external returns (bool) {
-        bytes32 hash = keccak256(byte(3), name.length, name, token);
-        assert(Valset.verify(hash, idxs, v, r, s));
-        cosmosTokenAddress[name] = new CosmosERC20(this, name);
-        Register(name, token);
-        return true;
-    }
-
-    function Peggy(address[] initAddress, uint64[] initPower) Valset(initAddress, initPower) { }
+    function Peggy(address[] initAddress, uint64[] initPower) { }
 }
