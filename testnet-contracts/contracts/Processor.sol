4325d1b6854f0a0c8b1f3893d4a11abbe88758fc
Aidan Musnitzky <musnit@gmail.com>
2019-09-16 13:33:32 -0700

    Update golang-ci version

--- testnet-contracts/contracts/Processor.sol
+++ testnet-contracts/contracts/Processor.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.5.0;
 
 import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "../../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "./TokenERC20.sol";
 
   /*
    *  @title: Processor
