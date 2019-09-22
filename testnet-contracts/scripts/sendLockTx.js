ca08b5854189175cbc06dcb4fb445c1f923eb143
denalimarsh <denalimarsh@gmail.com>
2019-09-22 01:08:53 -0700

    Updated peggy:lock with custom command line arguments

--- testnet-contracts/scripts/sendLockTx.js
+++ testnet-contracts/scripts/sendLockTx.js
@@ -12,12 +12,69 @@ module.exports = async () => {
   /*******************************************
    *** Constants
    ******************************************/
-  const COSMOS_RECIPIENT =
+  // Lock transaction default params
+  const DEFAULT_COSMOS_RECIPIENT =
     "0x636f736d6f7331706a74677530766175326d35326e72796b64707a74727438383761796b756530687137646668";
-  const COIN_DENOM = "0x0000000000000000000000000000000000000000";
-  const AMOUNT = 10;
+  const DEFAULT_COIN_DENOM = "0x0000000000000000000000000000000000000000";
+  const DEFAULT_AMOUNT = 10;
+
+  // Config values
   const NETWORK_ROPSTEN =
     process.argv[4] === "--network" && process.argv[5] === "ropsten";
+  const DEFAULT_PARAMS =
+    process.argv[4] === "--default" ||
+    (NETWORK_ROPSTEN && process.argv[6] === "--default");
+  const NUM_ARGS = process.argv.length - 4;
+
+  /*******************************************
+   *** Command line argument error checking
+   ***
+   *** truffle exec lacks support for dynamic command line arguments:
+   *** https://github.com/trufflesuite/truffle/issues/889#issuecomment-522581580
+   ******************************************/
+  if (NETWORK_ROPSTEN && DEFAULT_PARAMS) {
+    if (NUM_ARGS !== 3) {
+      return console.error(
+        "Error: custom parameters are invalid on --default."
+      );
+    }
+  } else if (NETWORK_ROPSTEN) {
+    if (NUM_ARGS !== 2 || 5) {
+      return console.error("Error: invalid parameters, please try again.");
+    }
+  } else if (DEFAULT_PARAMS) {
+    if (NUM_ARGS !== 1) {
+      return console.error(
+        "Error: custom parameters are invalid on --default."
+      );
+    }
+  } else {
+    if (NUM_ARGS !== 3) {
+      return console.error(
+        "Error: must specify recipient address, token address, and amount."
+      );
+    }
+  }
+
+  /*******************************************
+   *** Lock transaction parameters
+   ******************************************/
+  let cosmosRecipient = DEFAULT_COSMOS_RECIPIENT;
+  let coinDenom = DEFAULT_COIN_DENOM;
+  let amount = DEFAULT_AMOUNT;
+
+  // TODO: Input validation
+  if (!DEFAULT_PARAMS) {
+    if (NETWORK_ROPSTEN) {
+      cosmosRecipient = process.argv[6];
+      coinDenom = process.argv[7];
+      amount = parseInt(process.argv[8], 10);
+    } else {
+      cosmosRecipient = process.argv[4];
+      coinDenom = process.argv[5];
+      amount = parseInt(process.argv[6], 10);
+    }
+  }
 
   /*******************************************
    *** Web3 provider
@@ -44,9 +101,9 @@ module.exports = async () => {
 
   // Send lock transaction
   const { logs } = await contract.deployed().then(function(instance) {
-    return instance.lock(COSMOS_RECIPIENT, COIN_DENOM, AMOUNT, {
+    return instance.lock(cosmosRecipient, coinDenom, amount, {
       from: accounts[1],
-      value: AMOUNT,
+      value: amount,
       gas: 300000 // 300,000 Gwei
     });
   });
