24da98ba28bb0e675c38b47cbde17a6a1844f10f
denalimarsh <denalimarsh@gmail.com>
2019-09-21 23:36:15 -0700

    Changed ganache port to 7545, replaced npm with yarn

--- testnet-contracts/truffle-config.js
+++ testnet-contracts/truffle-config.js
@@ -6,9 +6,9 @@ module.exports = {
   networks: {
     develop: {
       host: "localhost",
-      port: 8545,
+      port: 7545,
       network_id: "*",
-      gas: 6000000,
+      gas: 6721975,
       gasPrice: 200000000000,
       solc: {
         version: "0.5.0",
@@ -18,11 +18,6 @@ module.exports = {
         }
       }
     },
-    ganache: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
     ropsten: {
       provider: function() {
         return new HDWalletProvider(
@@ -31,7 +26,7 @@ module.exports = {
         );
       },
       network_id: 3,
-      gas: 4700000
+      gas: 6000000
     }
   },
   rpc: {
