swishlabsco/peggy

Updated ethbridge with chainID, bridgeContractAddress, symbol, and tokenAddress. Updated chainID to type int from string.

x/ethbridge/client/cli/query.go
@@ -14,19 +14,25 @@ import (
 // GetCmdGetEthBridgeProphecy queries information about a specific prophecy
 func GetCmdGetEthBridgeProphecy(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "prophecy [nonce] [ethereum-sender]",
+		Use:   "prophecy [chain-id] [nonce] [ethereum-sender]",
 		Short: "Query prophecy",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
-			nonce, err := strconv.Atoi(args[0])
+			chainID, err := strconv.Atoi(args[0])
 			if err != nil {
 				return err
 			}
-			ethereumSender := types.NewEthereumAddress(args[1])
 
-			bz, err := cdc.MarshalJSON(ethbridge.NewQueryEthProphecyParams(nonce, ethereumSender))
+			nonce, err := strconv.Atoi(args[1])
+			if err != nil {
+				return err
+			}
+
+			ethereumSender := types.NewEthereumAddress(args[2])
+
+			bz, err := cdc.MarshalJSON(ethbridge.NewQueryEthProphecyParams(chainID, nonce, ethereumSender))
 			if err != nil {
 				return err
 			}

cosmos/gravity-bridge

x/ethbridge/client/cli/query.go
@@ -15,19 +15,25 @@ import (
 // GetCmdGetEthBridgeProphecy queries information about a specific prophecy
 func GetCmdGetEthBridgeProphecy(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "prophecy [nonce] [ethereum-sender]",
+		Use:   "prophecy [chain-id] [nonce] [ethereum-sender]",
 		Short: "Query prophecy",
 		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
-			nonce, err := strconv.Atoi(args[0])
+			chainID, err := strconv.Atoi(args[0])
 			if err != nil {
 				return err
 			}
-			ethereumSender := types.NewEthereumAddress(args[1])
 
-			bz, err := cdc.MarshalJSON(types.NewQueryEthProphecyParams(nonce, ethereumSender))
+			nonce, err := strconv.Atoi(args[1])
+			if err != nil {
+				return err
+			}
+
+			ethereumSender := types.NewEthereumAddress(args[2])
+
+			bz, err := cdc.MarshalJSON(types.NewQueryEthProphecyParams(chainID, nonce, ethereumSender))
 			if err != nil {
 				return err
 			}