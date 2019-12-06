09e83f0820c724570ebf9fd39f67f7728e51a958
denalimarsh <denalimarsh@gmail.com>
2019-12-06 14:47:16 -0800

    updated regex for golangci

--- cmd/ebrelayer/utils/utils.go
+++ cmd/ebrelayer/utils/utils.go
@@ -7,7 +7,6 @@ package utils
 // --------------------------------------------------------
 
 import (
-	"log"
 	"math/big"
 	"regexp"
 
@@ -31,10 +30,7 @@ func GetSymbolAmountFromCoin(coin string) (string, *big.Int) {
 	var symbol string
 
 	// Set up regex
-	isLetter, err := regexp.Compile(`[a-z]`)
-	if err != nil {
-		log.Fatal("Regex compilation error:", err)
-	}
+	isLetter := regexp.MustCompile(`[a-z]`)
 
 	// Iterate over each rune in the coin string
 	for i, char := range coinRune {
