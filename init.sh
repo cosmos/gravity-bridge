71b09e61f6f02eb8176a6f3ba42dc1cd4aac6eec
okwme <billy.rennekamp@gmail.com>
2020-01-31 12:06:20 +0100

    init tweak

--- init.sh
+++ init.sh
@@ -10,9 +10,14 @@ ebcli config trust-node true
 ebcli config indent true
 ebcli config output json
 
+# ebcli config keyring-backend file
+# (echo '1234567890'; echo '1234567890') | ebcli keys add validator
+# (echo '1234567890'; echo '1234567890') | ebcli keys add testuser
+# echo '1234567890' | ebd add-genesis-account $(ebcli keys show validator -a) 1000000000stake,1000000000atom
+
 ebcli keys add validator
 ebcli keys add testuser
-
 ebd add-genesis-account $(ebcli keys show validator -a) 1000000000stake,1000000000atom
+
 ebd gentx --name validator
 ebd collect-gentxs
