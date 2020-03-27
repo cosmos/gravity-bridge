241244cf6cb65c82086e8dac9a88d3f632f4cca2
okwme <billy.rennekamp@gmail.com>
2020-03-27 12:02:17 +0100

    remove flat config line

--- testnet-contracts/truffle-config.js
+++ testnet-contracts/truffle-config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 var HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-  contracts_directory: "./flat",
+  // contracts_directory: "./flat", // can be uncommented if generated flat contracts need to be tested
   networks: {
     develop: {
       host: "localhost",
