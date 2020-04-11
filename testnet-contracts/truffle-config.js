a03d8635f8ab2ef57f483af9224d6bf275de6a13
denalimarsh <denalimarsh@gmail.com>
2020-04-11 15:38:17 -0700

    sample createBridgeProphecy test

--- testnet-contracts/truffle-config.js
+++ testnet-contracts/truffle-config.js
@@ -5,45 +5,45 @@ var HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   // contracts_directory: "./flat",
   networks: {
-    develop: {
-      host: "localhost",
-      port: 7545, // Match default network 'ganache'
-      network_id: 5777,
-      gas: 6721975, // Truffle default development block gas limit
-      gasPrice: 200000000000,
-      solc: {
-        version: "0.5.0",
-        optimizer: {
-          enabled: true,
-          runs: 200
-        }
-      }
-    },
-    ropsten: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          "https://ropsten.infura.io/v3/".concat(process.env.INFURA_PROJECT_ID)
-        );
-      },
-      network_id: 3,
-      gas: 6000000
-    },
-    xdai: {
-      provider: function() {
-        return new HDWalletProvider(
-          process.env.MNEMONIC,
-          "https://dai.poa.network"
-        );
-      },
-      network_id: 100,
-      gas: 6000000
-    }
+    //   develop: {
+    //     host: "localhost",
+    //     port: 7545, // Match default network 'ganache'
+    //     network_id: "*",
+    //     gas: 6721975, // Truffle default development block gas limit
+    //     gasPrice: 200000000000,
+    //     solc: {
+    //       version: "0.5.0",
+    //       optimizer: {
+    //         enabled: true,
+    //         runs: 200
+    //       }
+    //     }
+    //   },
+    //   ropsten: {
+    //     provider: function() {
+    //       return new HDWalletProvider(
+    //         process.env.MNEMONIC,
+    //         "https://ropsten.infura.io/v3/".concat(process.env.INFURA_PROJECT_ID)
+    //       );
+    //     },
+    //     network_id: 3,
+    //     gas: 6000000
+    //   },
+    //   xdai: {
+    //     provider: function() {
+    //       return new HDWalletProvider(
+    //         process.env.MNEMONIC,
+    //         "https://dai.poa.network"
+    //       );
+    //     },
+    //     network_id: 100,
+    //     gas: 6000000
+    //   }
   },
-  rpc: {
-    host: "localhost",
-    post: 8080
-  },
+  // rpc: {
+  //   host: "localhost",
+  //   post: 8080
+  // },
   mocha: {
     useColors: true
   }
