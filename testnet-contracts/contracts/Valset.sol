2f1413cb9dd9458b0f91fdb16f9f44271e8915e2
okwme <billy.rennekamp@gmail.com>
2020-04-16 13:00:38 +0200

    config

--- testnet-contracts/contracts/Valset.sol
+++ testnet-contracts/contracts/Valset.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.0;
 
-import "../../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 
 contract Valset {
