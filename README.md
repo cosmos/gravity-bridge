c02775dc862c77068aad1d0bec40225ae177a4d7
Jehan <jehan.tremback@gmail.com>
2020-05-06 18:24:30 -0700

    convert to buidler

---
+++ README.md
@@ -0,0 +1,10 @@
+Run `npm install`, then `npm run typechain`.
+
+Run `npm run test` to run tests.
+
+After modifying solidity files, run `npm run typechain` to recompile contract
+typedefs.
+
+# Peggy
+
+https://docs.google.com/document/d/1eKAJ52wDsg-umDZsGbHR32yzhp9_7urzwrnmJGE6kRU/edit#heading=h.o9spmi6l357w
---
+++ buidler.config.ts
@@ -0,0 +1,29 @@
+import { usePlugin } from "@nomiclabs/buidler/config";
+usePlugin("@nomiclabs/buidler-waffle");
+usePlugin("buidler-gas-reporter");
+usePlugin("buidler-typechain");
+
+// // This is a sample Buidler task. To learn how to create your own go to
+// // https://buidler.dev/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, bre) => {
+//   const accounts = await bre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(await account.getAddress());
+//   }
+// });
+
+// You have to export an object to set up your config
+// This object can have the following optional entries:
+// defaultNetwork, networks, solc, and paths.
+// Go to https://buidler.dev/config/ to learn more
+module.exports = {
+  // This is a sample solc configuration that specifies which version of solc to use
+  solc: {
+    version: "0.5.15",
+  },
+  typechain: {
+    outDir: "typechain",
+    target: "ethers",
+  },
+};
