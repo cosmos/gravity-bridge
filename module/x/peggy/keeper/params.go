1986edc17be2881c6e6813b2a0939bc107fbdb4a
Justin Kilpatrick <justin@althea.net>
2020-05-14 11:16:12 -0400

    Boilerplate for Peggy module
    This compiles now, so that's progress

---
+++ module/x/peggy/keeper/params.go
@@ -0,0 +1,22 @@
+package keeper
+/*
+// TODO: Define if your module needs Parameters, if not this can be deleted
+
+import (
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/althea-net/peggy/module/x/peggy/types"
+)
+
+// GetParams returns the total set of peggy parameters.
+func (k Keeper) GetParams(ctx sdk.Context) (params types.Params) {
+	k.paramspace.GetParamSet(ctx, &params)
+	return params
+}
+
+// SetParams sets the peggy parameters to the param space.
+func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
+	k.paramspace.SetParamSet(ctx, &params)
+}
+*/
