f34b264dee5d4e8723a8e1b432456860d0f9eb45
Justin Kilpatrick <justin@althea.net>
2020-07-03 11:14:00 -0400

    Generalize integration-tests.sh for arbitrary nodes
    this adds a tiny little binary downloaded from the Althea updates
    server. This binary does nothing but generate a valid random ETH
    private key and print it.

--- tests/base.Dockerfile
+++ tests/base.Dockerfile
@@ -3,6 +3,8 @@ ENV GOPATH=/go
 ENV PATH=$PATH:/go/bin
 RUN dnf install -y git golang make gcc gcc-c++ which iproute iputils procps-ng vim tmux net-tools htop tar jq
 ADD https://gethstore.blob.core.windows.net/builds/geth-linux-amd64-1.9.14-6d74d1e5.tar.gz /geth/
+ADD https://updates.altheamesh.com/gen_eth_key /usr/bin/
+RUN chmod +x /usr/bin/gen_eth_key
 RUN cd /geth && tar -xvf * && mv /geth/**/geth /usr/bin/geth
 ARG REPOFOLDER
 ADD $REPOFOLDER /peggy
--- tests/integration-tests.sh
+++ tests/integration-tests.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-set -eu
+set -eux
 # WIP 
-
-NODE_1_IP="7.7.7.1"
-NODE_2_IP="7.7.7.2"
-NODE_3_IP="7.7.7.3"
-
-ETH_PRIVKEY_1="0x41b6fe18ea396208ab7dc526ca1cc59942b24df2d6436b6970fbe3a5d0c947a8"
-
-peggycli config node http://$NODE_1_IP:26657
-# Do node 1 crap
-peggycli tx nameservice update-eth-addr $ETH_PRIVKEY_1
+NODES=3
+for i in $(seq 1 $NODES);
+do
+NODE_IP="7.7.7.$i"
+ETH_PRIVKEY=$(gen_eth_key)
+echo "$ETH_PRIVKEY"
+peggycli config node http://$NODE_IP:26657
+peggycli tx nameservice update-eth-addr $ETH_PRIVKEY
+done
