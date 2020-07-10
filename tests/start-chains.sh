e4cc1761a1c5e27ddc6bd85555f2377b664e3917
Justin Kilpatrick <justin@althea.net>
2020-07-10 16:19:24 -0400

    Eliminate the second dockerfile
    This eliminate the second dockerfile, which is redundant since we
    also pull in Peggy using the mount.

--- tests/start-chains.sh
+++ tests/start-chains.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 set -eux
 
-# Build test container
-docker build --no-cache -f ./tests/tests.Dockerfile -t peggy-test .
-
 # Remove existing container instance
 set +e
 docker rm -f peggy_test_instance
 set -e
 
+NODES=3
+
 # Run new test container instance
-docker run --name peggy_test_instance --mount type=bind,source="$(pwd)"/,target=/peggy --cap-add=NET_ADMIN -it peggy-test
+docker run --name peggy_test_instance --mount type=bind,source="$(pwd)"/,target=/peggy --cap-add=NET_ADMIN -it peggy-base /bin/bash /peggy/tests/reload-code.sh $NODES
--- tests/tests.Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM peggy-base
-COPY . /peggy
-ENV NODES=3
-CMD pushd /peggy/ && tests/reload-code.sh $NODES
-# RUN pushd /peggy/module/ && make && make install
-# RUN pushd /peggy/ && tests/setup-validators.sh $NODES
-# CMD pushd /peggy/ && tests/run-testnet.sh $NODES
