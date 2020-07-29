6a620ddf57c630ce640e3bdd5f90edf15dc6e703
Jtremback <hi@jehan.email>
2020-07-29 20:55:38 +0000

    add Valset.GetCheckpoint stub

--- module/x/nameservice/types/types.go
+++ module/x/nameservice/types/types.go
@@ -8,8 +8,16 @@ import (
 )
 
 type Valset struct {
+	Nonce       int64
 	Powers      []int64
 	EthAdresses []string
+}
+
+func (v Valset) GetCheckpoint() []byte {
+	// Getting the equivalent of solidity's abi.encodePacked (or abi.encode) does not seem to be straightforward
+	// and I am skipping it for now to focus on the overall module structure
+	// https://stackoverflow.com/questions/50772811/how-can-i-get-the-same-return-value-as-solidity-abi-encodepacked-in-golang
+	return []byte("dothislater")
 }
 
 // MinNamePrice is Initial Starting Price for a name that was never previously owned
