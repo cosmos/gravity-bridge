2c84e5f72e7bbe4fd4354d7b34e909877a26a647
Justin Kilpatrick <justin@althea.net>
2020-08-13 17:00:50 -0400

    Use git archive for docker image
    We use a transparent mount in order to provide the container
    live access to the working directory. This means that it doesn't matter
    so much what's in the docker image for the development flow.
    
    This change dramaitcally reduces the total build time by excluding things
    like dependency caches, node_modules, and build directories from being
    copied over into the docker container state.
    
    This does affect the all up testing flow, requiring that changes be
    checked in before they can be tested. But it's still faster to check
    in your changes than it is to wait 45 seconds for docker to copy in
    all the useless build context.

--- tests/build-container.sh
+++ tests/build-container.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -eux
 
+# this directy of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DOCKERFOLDER=$DIR/dockerfile
+REPOFOLDER=$DIR/..
+
 #docker system prune -a -f
 # Build base container
-docker build -f ./tests/dockerfile/base.Dockerfile -t peggy-base .
+git archive --format=tar.gz -o $DOCKERFOLDER/peggy.tar.gz --prefix=peggy/ HEAD
+pushd $DOCKERFOLDER
+docker build -t peggy-base .
