8ca73981a731609c7356bcfe9b79a680f3f6e77e
Justin Kilpatrick <justin@althea.net>
2020-09-01 12:46:11 -0400

    Make test scripts directory independent
    This scripts now detect their own directory and change the pwd
    as required to keep things working.

--- tests/build-container.sh
+++ tests/build-container.sh
@@ -6,6 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOCKERFOLDER=$DIR/dockerfile
 REPOFOLDER=$DIR/..
 
+# change our directory sot hat the git arcive command works as expected
+pushd $REPOFOLDER
+
 #docker system prune -a -f
 # Build base container
 git archive --format=tar.gz -o $DOCKERFOLDER/peggy.tar.gz --prefix=peggy/ HEAD
--- tests/start-chains.sh
+++ tests/start-chains.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -eux
 
+# the directory of this script, useful for allowing this script
+# to be run with any PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Remove existing container instance
 set +e
 docker rm -f peggy_test_instance
 set -e
 
 NODES=3
+
+pushd $DIR/../
 
 # Run new test container instance
 docker run --name peggy_test_instance --mount type=bind,source="$(pwd)"/,target=/peggy --cap-add=NET_ADMIN -p 1317:1317 -it peggy-base /bin/bash /peggy/tests/container-scripts/reload-code.sh $NODES
