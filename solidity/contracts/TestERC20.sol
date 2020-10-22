f18a5f14ff1b76f981eb23ed829f9db50d4f739b
Justin Kilpatrick <justin@althea.net>
2020-10-22 12:24:25 -0400

    Issue the testnet miner address some ERC20 tokens
    This gives the testnet miner some tokens to use for bridge tests

--- solidity/contracts/TestERC20.sol
+++ solidity/contracts/TestERC20.sol
@@ -1,7 +1,6 @@
 pragma solidity ^0.6.6;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-
 // This is the coin we test with- the REAL Bitcoin
 contract TestERC20 is ERC20 {
 	constructor() public ERC20("Bitcoin MAX", "MAX") {
@@ -10,5 +9,9 @@ contract TestERC20 is ERC20 {
 		_mint(0xE5904695748fe4A84b40b3fc79De2277660BD1D3, 10000);
 		_mint(0x92561F28Ec438Ee9831D00D1D59fbDC981b762b2, 10000);
 		_mint(0x2fFd013AaA7B5a7DA93336C2251075202b33FB2B, 10000);
+		// this is the EtherBase address for our testnet miner in
+		// tests/assets/ETHGenesis.json so it wil have both a lot
+		// of ETH and a lot of erc20 tokens to test with
+		_mint(0xBf660843528035a5A4921534E156a27e64B231fE, 100000);
 	}
 }
