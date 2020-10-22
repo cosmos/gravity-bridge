b106347119f40cc76d781a2252d1167064c6af51
Jtremback <hi@jehan.email>
2020-10-22 14:11:25 -0700

    fix type errors

--- module/x/peggy/client/cli/attestation_tx.go
+++ module/x/peggy/client/cli/attestation_tx.go
@@ -351,7 +351,7 @@ func CmdOutgointTXBatchConfirm(storeKey string, cdc *codec.Codec) *cobra.Command
 
 			var batch types.OutgoingTxBatch
 			cdc.MustUnmarshalJSON(res, &batch)
-			checkpoint, err := batch.GetCheckpoint(&updateRsp.Valset)
+			checkpoint, err := batch.GetCheckpoint()
 			if err != nil {
 				return err
 			}
