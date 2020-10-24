1231f74692e4cdbb8b19dc2086f67ef9934a0162
Justin Kilpatrick <justin@althea.net>
2020-10-24 15:16:15 -0400

    Add client
    The client actually moves transactions across the bridge provided
    running validators and a network

--- orchestrator/orchestrator/src/client.rs
+++ orchestrator/orchestrator/src/client.rs
@@ -13,13 +13,15 @@ mod main_loop;
 mod tests;
 mod valset_relaying;
 
-use crate::main_loop::orchestrator_main_loop;
 use crate::main_loop::LOOP_SPEED;
 use clarity::Address as EthAddress;
 use clarity::PrivateKey as EthPrivateKey;
+use clarity::Uint256;
 use contact::client::Contact;
-use deep_space::private_key::PrivateKey as CosmosPrivateKey;
+use cosmos_peggy::send::send_valset_request;
+use deep_space::{coin::Coin, private_key::PrivateKey as CosmosPrivateKey};
 use docopt::Docopt;
+use ethereum_peggy::send_to_cosmos::send_to_cosmos;
 use std::time::Duration;
 use url::Url;
 use web30::client::Web3;
@@ -32,28 +34,34 @@ struct Args {
     flag_ethereum_rpc: String,
     flag_contract_address: String,
     flag_fees: String,
+    flag_amount: u128,
+    flag_erc20: String,
 }
 
 lazy_static! {
     pub static ref USAGE: String = format!(
-        "Usage: {} --cosmos-dest=<key> --erc20-address=<erc20> --amount=<amount> --contract-address=<addr> --ethereum-key=<key> --ethereum-rpc=<url>
+    "Usage: {} --cosmos-phrase=<key> --ethereum-key=<key> --cosmos-rpc=<url> --ethereum-rpc=<url> --fees=<denom> --contract-address=<addr>
         Options:
-            -h --help                     Show this screen.
-            --cosmos-dest=<ckey>   The Cosmos address of your destination
-            --erc20-address=<erc20>       The Ethereum contract address for the ERC20 token you want to send
-            --amount=<amount>             The number of ERC20 tokens you wish to send
-            --contract-address=<addr>     The Ethereum contract address for Peggy, this is temporary
-            --ethereum-key=<ekey>         The Ethereum private key with the funds you wish to send
-            --ethereum-rpc=<eurl>         The Ethereum RPC url, should be a self hosted node
+            -h --help                 Show this screen.
+            --cosmos-key=<ckey>       The Cosmos private key of the validator
+            --ethereum-key=<ekey>     The Ethereum private key of the validator
+            --cosmos-rpc=<curl>       The Cosmos RPC url, usually the validator
+            --ethereum-rpc=<eurl>     The Ethereum RPC url, should be a self hosted node
+            --fees=<denom>            The Cosmos Denom in which to pay Cosmos chain fees
+            --contract-address=<addr> The Ethereum contract address for Peggy, this is temporary
+            --erc20-address=<addr>    An erc20 address to send funds
+            --amount=<amount>
         About:
-            Client software for the Althea-Peggy bridge
+            Client software for Althea-Peggy.
             Written By: {}
             Version {}",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_AUTHORS"),
-        env!("CARGO_PKG_VERSION"),
-    );
+            env!("CARGO_PKG_NAME"),
+            env!("CARGO_PKG_AUTHORS"),
+            env!("CARGO_PKG_VERSION"),
+        );
 }
+
+const TIMEOUT: Duration = Duration::from_secs(60);
 
 #[actix_rt::main]
 async fn main() {
@@ -70,20 +78,48 @@ async fn main() {
         .flag_contract_address
         .parse()
         .expect("Invalid contract address!");
+    let erc20_address: EthAddress = args.flag_erc20.parse().expect("Invalid contract address!");
     let cosmos_url = Url::parse(&args.flag_cosmos_rpc).expect("Invalid Cosmos RPC url");
+    let cosmos_url = cosmos_url.to_string();
+    let cosmos_url = cosmos_url.trim_end_matches('/');
     let eth_url = Url::parse(&args.flag_ethereum_rpc).expect("Invalid Ethereum RPC url");
+    let eth_url = eth_url.to_string();
+    let eth_url = eth_url.trim_end_matches('/');
     let fee_denom = args.flag_fees;
 
-    let web3 = Web3::new(&eth_url.to_string(), LOOP_SPEED);
-    let contact = Contact::new(&cosmos_url.to_string(), LOOP_SPEED);
+    let web3 = Web3::new(&eth_url, LOOP_SPEED);
+    let contact = Contact::new(&cosmos_url, LOOP_SPEED);
+    let fee = Coin {
+        denom: fee_denom,
+        amount: 1u64.into(),
+    };
 
-    orchestrator_main_loop(
-        cosmos_key,
-        ethereum_key,
-        web3,
-        contact,
+    let cosmos_public_key = cosmos_key.to_public_key().unwrap().to_address();
+    let ethereum_public_key = ethereum_key.to_public_key().unwrap();
+
+    info!("Sending in valset request");
+    let _res = send_valset_request(&contact, cosmos_key, fee, None, None, None)
+        .await
+        .expect("Failed to send valset request");
+
+    let dest = cosmos_public_key;
+    let amount: Uint256 = 1u64.into();
+    info!(
+        "Sending to Cosmos from {} to {} with amount {}",
+        ethereum_public_key, dest, amount
+    );
+    // we send some erc20 tokens to the peggy contract to register a deposit
+    let tx_id = send_to_cosmos(
+        erc20_address,
         contract_address,
-        fee_denom,
+        amount.clone(),
+        dest,
+        ethereum_key,
+        Some(TIMEOUT),
+        &web3,
+        vec![],
     )
-    .await;
+    .await
+    .expect("Failed to send tokens to Cosmos");
+    info!("Send to Cosmos txid: {:#066x}", tx_id);
 }
