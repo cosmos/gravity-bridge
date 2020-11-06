b0d2aad6ca29efbdf3a4196ff0a6b55b9b37e32e
Justin Kilpatrick <justin@althea.net>
2020-11-06 15:28:00 -0500

    Kill test-runner each time integration test are run
    This will kill the old instance of the test runner so that it
    doesn't cause problems when trying to the integration tests back
    to back without running start-chains again.

--- tests/container-scripts/integration-tests.sh
+++ tests/container-scripts/integration-tests.sh
@@ -4,5 +4,9 @@ set -eu
 NODES=3 # Permanently set to 3 for now!
 QUERY_FLAGS="--home /validator1 --trace --node=http://7.7.7.1:26657 --chain-id=peggy-test -o=json"
 
+set +e
+killall -9 test-runner
+set -e
+
 pushd /peggy/orchestrator
 RUST_BACKTRACE=full RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --release --bin test-runner
