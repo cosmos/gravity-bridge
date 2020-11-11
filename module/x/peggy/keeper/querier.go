fe50a74262feb2d01507794ebacb3aa89a28b202
Jtremback <hi@jehan.email>
2020-11-10 16:20:03 -0800

    mark and move methods used by rust to the top

--- module/x/peggy/keeper/querier.go
+++ module/x/peggy/keeper/querier.go
@@ -8,16 +8,22 @@ import (
 	abci "github.com/tendermint/tendermint/abci/types"
 )
 
+// USED BY RUST:
+// "custom/%s/valsetRequest/%s"
+// "custom/%s/valsetConfirms/%s"
+// "custom/%s/lastValsetRequests"
+// "custom/%s/lastPendingValsetRequest/%s"
+
 const (
-	QueryCurrentValset                  = "currentValset"
-	QueryValsetRequest                  = "valsetRequest"
-	QueryValsetConfirm                  = "valsetConfirm"
-	QueryValsetConfirmsByNonce          = "valsetConfirms"
-	QueryLastValsetRequests             = "lastValsetRequests"
-	QueryLastPendingValsetRequestByAddr = "lastPendingValsetRequest"
-	QueryLastPendingBatchRequestByAddr  = "lastPendingBatchRequest"
-	QueryOutgoingTxBatches              = "allBatches"
-	QueryBridgedDenominators            = "allBridgedDenominators"
+	/* USED BY RUST */ QueryValsetRequest = "valsetRequest"
+	/* USED BY RUST */ QueryValsetConfirmsByNonce = "valsetConfirms"
+	/* USED BY RUST */ QueryLastValsetRequests = "lastValsetRequests"
+	/* USED BY RUST */ QueryLastPendingValsetRequestByAddr = "lastPendingValsetRequest"
+	QueryCurrentValset                                     = "currentValset"
+	QueryValsetConfirm                                     = "valsetConfirm"
+	QueryLastPendingBatchRequestByAddr                     = "lastPendingBatchRequest"
+	QueryOutgoingTxBatches                                 = "allBatches"
+	QueryBridgedDenominators                               = "allBridgedDenominators"
 )
 
 // NewQuerier is the module level router for state queries
@@ -48,16 +54,7 @@ func NewQuerier(keeper Keeper) sdk.Querier {
 	}
 }
 
-func queryCurrentValset(ctx sdk.Context, keeper Keeper) ([]byte, error) {
-	valset := keeper.GetCurrentValset(ctx)
-	res, err := codec.MarshalJSONIndent(keeper.cdc, valset)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
-
-	return res, nil
-}
-
+/* USED BY RUST */
 func queryValsetRequest(ctx sdk.Context, path []string, keeper Keeper) ([]byte, error) {
 	nonce, err := parseNonce(path[0])
 	if err != nil {
@@ -69,6 +66,90 @@ func queryValsetRequest(ctx sdk.Context, path []string, keeper Keeper) ([]byte, 
 		return nil, nil
 	}
 	res, err := codec.MarshalJSONIndent(keeper.cdc, *valset)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+/* USED BY RUST */
+// allValsetConfirmsByNonce returns all the confirm messages for a given nonce
+// When nothing found an empty json array is returned. No pagination.
+func allValsetConfirmsByNonce(ctx sdk.Context, nonceStr string, keeper Keeper) ([]byte, error) {
+	nonce, err := parseNonce(nonceStr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
+	}
+
+	var confirms []types.MsgValsetConfirm
+	keeper.IterateValsetConfirmByNonce(ctx, nonce, func(_ []byte, c types.MsgValsetConfirm) bool {
+		confirms = append(confirms, c)
+		return false
+	})
+	if len(confirms) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(keeper.cdc, confirms)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+
+	return res, nil
+}
+
+const maxValsetRequestsReturned = 5
+
+/* USED BY RUST */
+func lastValsetRequests(ctx sdk.Context, keeper Keeper) ([]byte, error) {
+	var counter int
+	var valReq []types.Valset
+	keeper.IterateValsetRequest(ctx, func(_ []byte, val types.Valset) bool {
+		valReq = append(valReq, val)
+		counter++
+		return counter >= maxValsetRequestsReturned
+	})
+	if len(valReq) == 0 {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(keeper.cdc, valReq)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
+}
+
+/* USED BY RUST */
+func lastPendingValsetRequest(ctx sdk.Context, operatorAddr string, keeper Keeper) ([]byte, error) {
+	addr, err := sdk.AccAddressFromBech32(operatorAddr)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
+	}
+
+	// todo: find validator address by operator key
+	validatorAddr := sdk.ValAddress(addr)
+
+	var pendingValsetReq *types.Valset
+	keeper.IterateValsetRequest(ctx, func(_ []byte, val types.Valset) bool {
+		found := keeper.HasValsetApprovalSignature(ctx, val.Nonce, validatorAddr)
+		if !found {
+			pendingValsetReq = &val
+		}
+		return true
+	})
+	if pendingValsetReq == nil {
+		return nil, nil
+	}
+	res, err := codec.MarshalJSONIndent(keeper.cdc, pendingValsetReq)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return res, nil
+}
+
+func queryCurrentValset(ctx sdk.Context, keeper Keeper) ([]byte, error) {
+	valset := keeper.GetCurrentValset(ctx)
+	res, err := codec.MarshalJSONIndent(keeper.cdc, valset)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
@@ -98,77 +179,6 @@ func queryValsetConfirm(ctx sdk.Context, path []string, keeper Keeper) ([]byte, 
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
 
-	return res, nil
-}
-
-// allValsetConfirmsByNonce returns all the confirm messages for a given nonce
-// When nothing found an empty json array is returned. No pagination.
-func allValsetConfirmsByNonce(ctx sdk.Context, nonceStr string, keeper Keeper) ([]byte, error) {
-	nonce, err := parseNonce(nonceStr)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, err.Error())
-	}
-
-	var confirms []types.MsgValsetConfirm
-	keeper.IterateValsetConfirmByNonce(ctx, nonce, func(_ []byte, c types.MsgValsetConfirm) bool {
-		confirms = append(confirms, c)
-		return false
-	})
-	if len(confirms) == 0 {
-		return nil, nil
-	}
-	res, err := codec.MarshalJSONIndent(keeper.cdc, confirms)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
-
-	return res, nil
-}
-
-const maxValsetRequestsReturned = 5
-
-func lastValsetRequests(ctx sdk.Context, keeper Keeper) ([]byte, error) {
-	var counter int
-	var valReq []types.Valset
-	keeper.IterateValsetRequest(ctx, func(_ []byte, val types.Valset) bool {
-		valReq = append(valReq, val)
-		counter++
-		return counter >= maxValsetRequestsReturned
-	})
-	if len(valReq) == 0 {
-		return nil, nil
-	}
-	res, err := codec.MarshalJSONIndent(keeper.cdc, valReq)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
-	return res, nil
-}
-
-func lastPendingValsetRequest(ctx sdk.Context, operatorAddr string, keeper Keeper) ([]byte, error) {
-	addr, err := sdk.AccAddressFromBech32(operatorAddr)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidRequest, "address invalid")
-	}
-
-	// todo: find validator address by operator key
-	validatorAddr := sdk.ValAddress(addr)
-
-	var pendingValsetReq *types.Valset
-	keeper.IterateValsetRequest(ctx, func(_ []byte, val types.Valset) bool {
-		found := keeper.HasValsetApprovalSignature(ctx, val.Nonce, validatorAddr)
-		if !found {
-			pendingValsetReq = &val
-		}
-		return true
-	})
-	if pendingValsetReq == nil {
-		return nil, nil
-	}
-	res, err := codec.MarshalJSONIndent(keeper.cdc, pendingValsetReq)
-	if err != nil {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
-	}
 	return res, nil
 }
 
