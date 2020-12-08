e2bc63a5e964df7c32c3bd8ee3d9b452eca202b2
Justin Kilpatrick <justin@althea.net>
2020-12-08 10:40:22 -0500

    Use async delay_for when waiting for blocks
    This keeps execution time outside of the main thread and adds
    timeouts incase the blockchain halts.

--- orchestrator/cosmos_peggy/src/utils.rs
+++ orchestrator/cosmos_peggy/src/utils.rs
@@ -1,8 +1,10 @@
 use contact::client::Contact;
 use std::time::Duration;
-use std::{thread, time::Instant};
+use std::time::Instant;
+use tokio::time::delay_for;
 
-pub async fn wait_for_next_cosmos_block(contact: &Contact) {
+pub async fn wait_for_next_cosmos_block(contact: &Contact, timeout: Duration) {
+    let start = Instant::now();
     let current_block = contact
         .get_latest_block()
         .await
@@ -19,7 +21,10 @@ pub async fn wait_for_next_cosmos_block(contact: &Contact) {
             .last_commit
             .height
     {
-        thread::sleep(Duration::from_secs(1))
+        delay_for(Duration::from_secs(1)).await;
+        if Instant::now() - start > timeout {
+            panic!("Cosmos chain took too long to produce a block!");
+        }
     }
 }
 
@@ -27,10 +32,13 @@ pub async fn wait_for_cosmos_online(contact: &Contact, timeout: Duration) {
     let start = Instant::now();
     let mut current_block = contact.get_latest_block().await;
     while current_block.is_err() {
-        thread::sleep(Duration::from_secs(1));
+        delay_for(Duration::from_secs(1)).await;
         current_block = contact.get_latest_block().await;
         if Instant::now() - start > timeout {
             panic!("Cosmos chain took too long to start!");
         }
     }
+    // we have a block now, wait for a few more.
+    wait_for_next_cosmos_block(contact, timeout).await;
+    wait_for_next_cosmos_block(contact, timeout).await;
 }
