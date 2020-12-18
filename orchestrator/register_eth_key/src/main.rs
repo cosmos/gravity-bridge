158cd65e811820ab0b2cf62a68ca65278ebceaea
Justin Kilpatrick <justin@althea.net>
2020-12-18 07:55:17 -0500

    Allow register_eth_key to generate an Ethereum key
    This is a simple patch that adds the optional capability to generate
    an Ethereum key to register_eth_key. This provides a lot of utility
    to those that don't otherwise have the ability to generate some
    random addresses easily.

--- orchestrator/register_eth_key/src/main.rs
+++ orchestrator/register_eth_key/src/main.rs
@@ -4,8 +4,6 @@
 extern crate serde_derive;
 #[macro_use]
 extern crate lazy_static;
-#[macro_use]
-extern crate log;
 
 use std::time::Duration;
 
@@ -14,27 +12,31 @@ use contact::client::Contact;
 use cosmos_peggy::send::update_peggy_eth_address;
 use deep_space::{coin::Coin, private_key::PrivateKey as CosmosPrivateKey};
 use docopt::Docopt;
+use rand::{thread_rng, Rng};
 use url::Url;
 
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_cosmos_phrase: String,
-    flag_ethereum_key: String,
+    flag_ethereum_key: Option<String>,
     flag_cosmos_rpc: String,
     flag_fees: String,
 }
 
 lazy_static! {
     pub static ref USAGE: String = format!(
-        "Usage: {} --cosmos-phrase=<key> --ethereum-key=<key> --cosmos-rpc=<url> --fees=<denom>
+        "Usage: {} --cosmos-phrase=<key> [--ethereum-key=<key>] --cosmos-rpc=<url> --fees=<denom>
         Options:
             -h --help                     Show this screen.
-            --cosmos-phrase=<ckey>    The Cosmos private key of the validator
-            --ethereum-key=<ekey>     The Ethereum private key of the validator
-            --cosmos-rpc=<curl>       The Cosmos RPC url, usually the validator
+            --cosmos-phrase=<ckey>    The Cosmos private key of the validator. Must be saved when you generate your key
+            --ethereum-key=<ekey>     (Optional) The Ethereum private key to register, will be generated if not provided
+            --cosmos-rpc=<curl>       The Cosmos Legacy RPC url, usually the validator. This will need to be manually enabled
             --fees=<denom>            The Cosmos Denom in which to pay Cosmos chain fees
         About:
-            Client software for the Althea-Peggy bridge
+            Special purpose binary for bootstrapping Peggy chains. This will submit and optionally
+            generate an Ethereum key that will be used to sign messages on behalf of your Validator
+            on the Cosmos blockchain running the Peggy module. Be aware this Ethereum key must be kept
+            safe as you can be slashed for losing it.
             Written By: {}
             Version {}",
         env!("CARGO_PKG_NAME"),
@@ -57,10 +59,19 @@ async fn main() {
         .unwrap_or_else(|e| e.exit());
     let cosmos_key = CosmosPrivateKey::from_phrase(&args.flag_cosmos_phrase, "")
         .expect("Failed to parse validator key");
-    let ethereum_key: EthPrivateKey = args
-        .flag_ethereum_key
-        .parse()
-        .expect("Invalid Ethereum private key!");
+    let ethereum_key = if let Some(key) = args.flag_ethereum_key {
+        key.parse().expect("Invalid Ethereum Private key!")
+    } else {
+        let mut rng = thread_rng();
+        let key: [u8; 32] = rng.gen();
+        let key = EthPrivateKey::from_slice(&key).unwrap();
+        println!(
+            "No Ethereum key provided, your generated key is {} -> {}",
+            key,
+            key.to_public_key().unwrap()
+        );
+        key
+    };
     let cosmos_url = Url::parse(&args.flag_cosmos_rpc).expect("Invalid Cosmos RPC url");
     let cosmos_url = cosmos_url.to_string();
     let cosmos_url = cosmos_url.trim_end_matches('/');
@@ -78,7 +89,7 @@ async fn main() {
 
     let eth_address = ethereum_key.to_public_key().unwrap();
     let cosmos_address = cosmos_key.to_public_key().unwrap().to_address();
-    info!(
+    println!(
         "Registered Ethereum address {} for validator address {}",
         eth_address, cosmos_address
     )
