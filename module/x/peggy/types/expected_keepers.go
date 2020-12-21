dce030eab2350e06e52b76f97d2cd43b3bb98792
Jack Zampolin <jack.zampolin@gmail.com>
2020-12-21 09:51:16 -0800

    Add additional staking keeper functions
    Co-authored-by: Justin Kilpatrick <justin@althea.net>

--- module/x/peggy/types/expected_keepers.go
+++ module/x/peggy/types/expected_keepers.go
@@ -10,6 +10,15 @@ type StakingKeeper interface {
 	GetBondedValidatorsByPower(ctx sdk.Context) []stakingtypes.Validator
 	GetLastValidatorPower(ctx sdk.Context, operator sdk.ValAddress) int64
 	GetLastTotalPower(ctx sdk.Context) (power sdk.Int)
+	IterateValidators(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
+	IterateBondedValidatorsByPower(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
+	IterateLastValidators(sdk.Context, func(index int64, validator stakingtypes.ValidatorI) (stop bool))
+	Validator(sdk.Context, sdk.ValAddress) stakingtypes.ValidatorI
+	ValidatorByConsAddr(sdk.Context, sdk.ConsAddress) stakingtypes.ValidatorI
+	Slash(sdk.Context, sdk.ConsAddress, int64, int64, sdk.Dec)
+	Jail(sdk.Context, sdk.ConsAddress)
+	Delegation(sdk.Context, sdk.AccAddress, sdk.ValAddress) stakingtypes.DelegationI
+	MaxValidators(sdk.Context) uint32
 }
 
 // BankKeeper defines the expected bank keeper methods
