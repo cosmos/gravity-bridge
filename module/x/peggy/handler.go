8a04e789d11bd16f0594cbb9446f11cb8c6f06e1
Jack Zampolin <jack.zampolin@gmail.com>
2020-12-21 16:28:32 -0800

    Move handler to msg_server
    This is a mirror of upstream changes in the CosmosSDK where generated
    protobuf code is used to actually generate the handler for each message
    type and we can simply write implementations. See the following ADR
    
    https://github.com/cosmos/cosmos-sdk/blob/master/docs/architecture/adr-031-msg-service.md
    
    Co-authored-by: Justin Kilpatrick <justin@althea.net>

--- module/x/peggy/handler.go
+++ module/x/peggy/handler.go
@@ -1,9 +1,7 @@
 package peggy
 
 import (
-	"encoding/hex"
 	"fmt"
-	"strings"
 
 	"github.com/althea-net/peggy/module/x/peggy/keeper"
 	"github.com/althea-net/peggy/module/x/peggy/types"
@@ -12,266 +10,38 @@ import (
 )
 
 // NewHandler returns a handler for "Peggy" type messages.
-func NewHandler(keeper keeper.Keeper) sdk.Handler {
+func NewHandler(k keeper.Keeper) sdk.Handler {
+	msgServer := keeper.NewMsgServerImpl(k)
+
 	return func(ctx sdk.Context, msg sdk.Msg) (*sdk.Result, error) {
+		ctx = ctx.WithEventManager(sdk.NewEventManager())
 		switch msg := msg.(type) {
 		case *types.MsgSetEthAddress:
-			return handleMsgSetEthAddress(ctx, keeper, msg)
+			res, err := msgServer.SetEthAddress(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgValsetConfirm:
-			return handleMsgConfirmValset(ctx, keeper, msg)
+			res, err := msgServer.ValsetConfirm(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgValsetRequest:
-			return handleMsgValsetRequest(ctx, keeper, msg)
+			res, err := msgServer.ValsetRequest(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgSendToEth:
-			return handleMsgSendToEth(ctx, keeper, msg)
+			res, err := msgServer.SendToEth(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgRequestBatch:
-			return handleMsgRequestBatch(ctx, keeper, msg)
+			res, err := msgServer.RequestBatch(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgConfirmBatch:
-			return handleMsgConfirmBatch(ctx, keeper, msg)
+			res, err := msgServer.ConfirmBatch(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgDepositClaim:
-			return handleDepositClaim(ctx, keeper, msg)
+			res, err := msgServer.DepositClaim(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		case *types.MsgWithdrawClaim:
-			return handleWithdrawClaim(ctx, keeper, msg)
+			res, err := msgServer.WithdrawClaim(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, fmt.Sprintf("Unrecognized Peggy Msg type: %v", msg.Type()))
 		}
 	}
-}
-
-func handleDepositClaim(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgDepositClaim) (*sdk.Result, error) {
-	var attestationIDs []string
-	orch, _ := sdk.AccAddressFromBech32(msg.Orchestrator)
-	validator := findValidatorKey(ctx, orch)
-	if validator == nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
-	}
-	if !keeper.StakingKeeper.Validator(ctx, validator).IsBonded() {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
-	}
-	att, err := keeper.AddClaim(ctx, msg.GetType(), msg.GetEventNonce(), validator, msg)
-	if err != nil {
-		return nil, sdkerrors.Wrap(err, "create attestation")
-	}
-	attestationIDs = append(attestationIDs, fmt.Sprint(types.GetAttestationKey(att.EventNonce, msg)))
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyAttestationIDs, strings.Join(attestationIDs, ",")),
-		),
-	)
-
-	return &sdk.Result{}, nil
-}
-
-func handleWithdrawClaim(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgWithdrawClaim) (*sdk.Result, error) {
-	var attestationIDs []string
-	orch, _ := sdk.AccAddressFromBech32(msg.Orchestrator)
-	validator := findValidatorKey(ctx, orch)
-	if validator == nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
-	}
-	if !keeper.StakingKeeper.Validator(ctx, validator).IsBonded() {
-		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
-	}
-	att, err := keeper.AddClaim(ctx, msg.GetType(), msg.GetEventNonce(), validator, msg)
-	if err != nil {
-		return nil, sdkerrors.Wrap(err, "create attestation")
-	}
-	attestationIDs = append(attestationIDs, string(types.GetAttestationKey(att.EventNonce, msg)))
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyAttestationIDs, strings.Join(attestationIDs, ",")),
-		),
-	)
-
-	return &sdk.Result{}, nil
-}
-
-func findValidatorKey(ctx sdk.Context, orchAddr sdk.AccAddress) sdk.ValAddress {
-	// todo: implement proper in keeper
-	// TODO: do we want ValAddress or do we want the AccAddress for the validator?
-	// this is a v important question for encoding
-	return sdk.ValAddress(orchAddr)
-}
-
-func handleMsgValsetRequest(ctx sdk.Context, k keeper.Keeper, msg *types.MsgValsetRequest) (*sdk.Result, error) {
-	req, err := sdk.AccAddressFromBech32(msg.Requester)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
-	}
-
-	val := k.StakingKeeper.Validator(ctx, findValidatorKey(ctx, req))
-	switch {
-	case val == nil:
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "validator not in valset")
-	case !val.IsBonded():
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "validator not bonded")
-	}
-
-	// disabling bootstrap check for integration tests to pass
-	//if keeper.GetLastValsetObservedNonce(ctx).isValid() {
-	//	return nil, sdkerrors.Wrap(types.ErrInvalid, "bridge bootstrap process not observed, yet")
-	//}
-	v := k.SetValsetRequest(ctx)
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyValsetNonce, fmt.Sprint(v.Nonce)),
-		),
-	)
-	return &sdk.Result{}, nil
-}
-
-// This function takes in a signature submitted by a validator's Eth Signer
-func handleMsgConfirmBatch(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgConfirmBatch) (*sdk.Result, error) {
-
-	batch := keeper.GetOutgoingTXBatch(ctx, msg.TokenContract, msg.Nonce)
-	if batch == nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "couldn't find batch")
-	}
-
-	peggyID := keeper.GetPeggyID(ctx)
-	checkpoint, err := batch.GetCheckpoint(peggyID)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "checkpoint generation")
-	}
-
-	sigBytes, err := hex.DecodeString(msg.Signature)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "signature decoding")
-	}
-	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
-	validator := findValidatorKey(ctx, valaddr)
-	if validator == nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "validator")
-	}
-
-	ethAddress := keeper.GetEthAddress(ctx, sdk.AccAddress(validator))
-	if ethAddress == "" {
-		return nil, sdkerrors.Wrap(types.ErrEmpty, "eth address")
-	}
-	err = types.ValidateEthereumSignature(checkpoint, sigBytes, ethAddress)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, fmt.Sprintf("signature verification failed expected sig by %s with peggy-id %s with checkpoint %s found %s", ethAddress, peggyID, hex.EncodeToString(checkpoint), msg.Signature))
-	}
-
-	// check if we already have this confirm
-	if keeper.GetBatchConfirm(ctx, msg.Nonce, msg.TokenContract, valaddr) != nil {
-		return nil, sdkerrors.Wrap(types.ErrDuplicate, "signature duplicate")
-	}
-	key := keeper.SetBatchConfirm(ctx, msg)
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyBatchConfirmKey, string(key)),
-		),
-	)
-
-	return &sdk.Result{}, nil
-}
-
-// This function takes in a signature submitted by a validator's Eth Signer
-func handleMsgConfirmValset(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgValsetConfirm) (*sdk.Result, error) {
-
-	valset := keeper.GetValsetRequest(ctx, msg.Nonce)
-	if valset == nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "couldn't find valset")
-	}
-
-	peggyID := keeper.GetPeggyID(ctx)
-	checkpoint := valset.GetCheckpoint(peggyID)
-
-	sigBytes, err := hex.DecodeString(msg.Signature)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, "signature decoding")
-	}
-	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
-	validator := findValidatorKey(ctx, valaddr)
-	if validator == nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "validator")
-	}
-
-	ethAddress := keeper.GetEthAddress(ctx, sdk.AccAddress(validator))
-	if ethAddress == "" {
-		return nil, sdkerrors.Wrap(types.ErrEmpty, "eth address")
-	}
-	err = types.ValidateEthereumSignature(checkpoint, sigBytes, ethAddress)
-	if err != nil {
-		return nil, sdkerrors.Wrap(types.ErrInvalid, fmt.Sprintf("signature verification failed expected sig by %s with peggy-id %s with checkpoint %s found %s", ethAddress, peggyID, hex.EncodeToString(checkpoint), msg.Signature))
-	}
-
-	// persist signature
-	if keeper.GetValsetConfirm(ctx, msg.Nonce, valaddr) != nil {
-		return nil, sdkerrors.Wrap(types.ErrDuplicate, "signature duplicate")
-	}
-	key := keeper.SetValsetConfirm(ctx, *msg)
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyValsetConfirmKey, string(key)),
-		),
-	)
-
-	return &sdk.Result{}, nil
-}
-
-func handleMsgSetEthAddress(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgSetEthAddress) (*sdk.Result, error) {
-	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
-	validator := findValidatorKey(ctx, valaddr)
-	if validator == nil {
-		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
-	}
-
-	keeper.SetEthAddress(ctx, sdk.AccAddress(validator), msg.Address)
-	return &sdk.Result{}, nil
-}
-
-func handleMsgSendToEth(ctx sdk.Context, keeper keeper.Keeper, msg *types.MsgSendToEth) (*sdk.Result, error) {
-	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
-	txID, err := keeper.AddToOutgoingPool(ctx, sender, msg.EthDest, msg.Amount, msg.BridgeFee)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyOutgoingTXID, fmt.Sprint(txID)),
-		),
-	)
-
-	return &sdk.Result{}, nil
-}
-
-func handleMsgRequestBatch(ctx sdk.Context, k keeper.Keeper, msg *types.MsgRequestBatch) (*sdk.Result, error) {
-	// ensure that peggy denom is valid
-	ec, err := types.ERC20FromPeggyCoin(sdk.NewInt64Coin(msg.Denom, 0))
-	if err != nil {
-		return nil, sdkerrors.Wrapf(types.ErrInvalid, "invalid denom: %s", err)
-	}
-
-	batchID, err := k.BuildOutgoingTXBatch(ctx, ec.Contract, keeper.OutgoingTxBatchSize)
-	if err != nil {
-		return nil, err
-	}
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			sdk.EventTypeMessage,
-			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
-			sdk.NewAttribute(types.AttributeKeyBatchNonce, fmt.Sprint(batchID.BatchNonce)),
-		),
-	)
-
-	return &sdk.Result{}, nil
 }
--- module/x/peggy/keeper/msg_server.go
+++ module/x/peggy/keeper/msg_server.go
@@ -2,8 +2,12 @@ package keeper
 
 import (
 	"context"
+	"encoding/hex"
+	"fmt"
 
 	"github.com/althea-net/peggy/module/x/peggy/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 type msgServer struct {
@@ -18,40 +22,285 @@ func NewMsgServerImpl(keeper Keeper) types.MsgServer {
 
 var _ types.MsgServer = msgServer{}
 
-// ValsetConfirm
+// ValsetConfirm handles MsgValsetConfirm
 func (k msgServer) ValsetConfirm(c context.Context, msg *types.MsgValsetConfirm) (*types.MsgValsetConfirmResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	// fetch the ValsetRequest by nonce
+	valset := k.GetValsetRequest(ctx, msg.Nonce)
+	if valset == nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "couldn't find valset")
+	}
+
+	peggyID := k.GetPeggyID(ctx)
+	checkpoint := valset.GetCheckpoint(peggyID)
+
+	sigBytes, err := hex.DecodeString(msg.Signature)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "signature decoding")
+	}
+	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
+	validator := findValidatorKey(ctx, valaddr)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "validator")
+	}
+
+	ethAddress := k.GetEthAddress(ctx, sdk.AccAddress(validator))
+	if ethAddress == "" {
+		return nil, sdkerrors.Wrap(types.ErrEmpty, "eth address")
+	}
+	err = types.ValidateEthereumSignature(checkpoint, sigBytes, ethAddress)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, fmt.Sprintf("signature verification failed expected sig by %s with peggy-id %s with checkpoint %s found %s", ethAddress, peggyID, hex.EncodeToString(checkpoint), msg.Signature))
+	}
+
+	// persist signature
+	if k.GetValsetConfirm(ctx, msg.Nonce, valaddr) != nil {
+		return nil, sdkerrors.Wrap(types.ErrDuplicate, "signature duplicate")
+	}
+	key := k.SetValsetConfirm(ctx, *msg)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyValsetConfirmKey, string(key)),
+		),
+	)
+
+	return &types.MsgValsetConfirmResponse{}, nil
 }
 
-// ValsetRequest
+// ValsetRequest handles MsgValsetRequest
 func (k msgServer) ValsetRequest(c context.Context, msg *types.MsgValsetRequest) (*types.MsgValsetRequestResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	// return an error if the requester address isn't valid
+	req, _ := sdk.AccAddressFromBech32(msg.Requester)
+
+	// return an error if the validator key doesn't exist
+	validator := findValidatorKey(ctx, req)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
+	}
+
+	// return an error if the validator isn't in the active set
+	val := k.StakingKeeper.Validator(ctx, validator)
+	switch {
+	case val == nil:
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	case !val.IsBonded():
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	}
+
+	// disabling bootstrap check for integration tests to pass
+	//if keeper.GetLastValsetObservedNonce(ctx).isValid() {
+	//	return nil, sdkerrors.Wrap(types.ErrInvalid, "bridge bootstrap process not observed, yet")
+	//}
+	v := k.SetValsetRequest(ctx)
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyValsetNonce, fmt.Sprint(v.Nonce)),
+		),
+	)
+	return &types.MsgValsetRequestResponse{}, nil
 }
 
 // SetEthAddress
 func (k msgServer) SetEthAddress(c context.Context, msg *types.MsgSetEthAddress) (*types.MsgSetEthAddressResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
+	validator := findValidatorKey(ctx, valaddr)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
+	}
+
+	k.Keeper.SetEthAddress(ctx, sdk.AccAddress(validator), msg.Address)
+	return &types.MsgSetEthAddressResponse{}, nil
 }
 
 // SendToEth
 func (k msgServer) SendToEth(c context.Context, msg *types.MsgSendToEth) (*types.MsgSendToEthResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+	sender, _ := sdk.AccAddressFromBech32(msg.Sender)
+	txID, err := k.AddToOutgoingPool(ctx, sender, msg.EthDest, msg.Amount, msg.BridgeFee)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyOutgoingTXID, fmt.Sprint(txID)),
+		),
+	)
+
+	return &types.MsgSendToEthResponse{}, nil
 }
 
 // RequestBatch
 func (k msgServer) RequestBatch(c context.Context, msg *types.MsgRequestBatch) (*types.MsgRequestBatchResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+	// TODO: don't we do this in validate basic?
+	// ensure that peggy denom is valid
+	ec, err := types.ERC20FromPeggyCoin(sdk.NewInt64Coin(msg.Denom, 0))
+	if err != nil {
+		return nil, sdkerrors.Wrapf(types.ErrInvalid, "invalid denom: %s", err)
+	}
+
+	batchID, err := k.BuildOutgoingTXBatch(ctx, ec.Contract, OutgoingTxBatchSize)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyBatchNonce, fmt.Sprint(batchID.BatchNonce)),
+		),
+	)
+
+	return &types.MsgRequestBatchResponse{}, nil
 }
 
 // ConfirmBatch
 func (k msgServer) ConfirmBatch(c context.Context, msg *types.MsgConfirmBatch) (*types.MsgConfirmBatchResponse, error) {
+	ctx := sdk.UnwrapSDKContext(c)
+
+	// fetch the outgoing batch given the nonce
+	batch := k.GetOutgoingTXBatch(ctx, msg.TokenContract, msg.Nonce)
+	if batch == nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "couldn't find batch")
+	}
+
+	peggyID := k.GetPeggyID(ctx)
+	checkpoint, err := batch.GetCheckpoint(peggyID)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "checkpoint generation")
+	}
+
+	sigBytes, err := hex.DecodeString(msg.Signature)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, "signature decoding")
+	}
+	valaddr, _ := sdk.AccAddressFromBech32(msg.Validator)
+	validator := findValidatorKey(ctx, valaddr)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "validator")
+	}
+
+	ethAddress := k.GetEthAddress(ctx, sdk.AccAddress(validator))
+	if ethAddress == "" {
+		return nil, sdkerrors.Wrap(types.ErrEmpty, "eth address")
+	}
+	err = types.ValidateEthereumSignature(checkpoint, sigBytes, ethAddress)
+	if err != nil {
+		return nil, sdkerrors.Wrap(types.ErrInvalid, fmt.Sprintf("signature verification failed expected sig by %s with peggy-id %s with checkpoint %s found %s", ethAddress, peggyID, hex.EncodeToString(checkpoint), msg.Signature))
+	}
+
+	// check if we already have this confirm
+	if k.GetBatchConfirm(ctx, msg.Nonce, msg.TokenContract, valaddr) != nil {
+		return nil, sdkerrors.Wrap(types.ErrDuplicate, "signature duplicate")
+	}
+	key := k.SetBatchConfirm(ctx, msg)
+
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			sdk.NewAttribute(types.AttributeKeyBatchConfirmKey, string(key)),
+		),
+	)
+
 	return nil, nil
 }
 
 func (k msgServer) DepositClaim(c context.Context, msg *types.MsgDepositClaim) (*types.MsgDepositClaimResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	// return an error if the orchestrator address isn't a valid SDK address
+	orch, _ := sdk.AccAddressFromBech32(msg.Orchestrator)
+
+	// return an error if the validator key doesn't exist
+	validator := findValidatorKey(ctx, orch)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
+	}
+
+	// return an error if the validator isn't in the active set
+	val := k.StakingKeeper.Validator(ctx, validator)
+	switch {
+	case val == nil:
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	case !val.IsBonded():
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	}
+
+	// Add the claim to the store
+	att, err := k.AddClaim(ctx, msg.GetType(), msg.GetEventNonce(), validator, msg)
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "create attestation")
+	}
+
+	// Emit the handle message event
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			// TODO: maybe return something better here? is this the right string representation?
+			sdk.NewAttribute(types.AttributeKeyAttestationID, string(types.GetAttestationKey(att.EventNonce, msg))),
+		),
+	)
+
+	return &types.MsgDepositClaimResponse{}, nil
 }
 
 func (k msgServer) WithdrawClaim(c context.Context, msg *types.MsgWithdrawClaim) (*types.MsgWithdrawClaimResponse, error) {
-	return nil, nil
+	ctx := sdk.UnwrapSDKContext(c)
+
+	orch, _ := sdk.AccAddressFromBech32(msg.Orchestrator)
+
+	// return an error if the validator key doesn't exist
+	validator := findValidatorKey(ctx, orch)
+	if validator == nil {
+		return nil, sdkerrors.Wrap(types.ErrUnknown, "address")
+	}
+
+	// return an error if the validator isn't in the active set
+	val := k.StakingKeeper.Validator(ctx, validator)
+	switch {
+	case val == nil:
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	case !val.IsBonded():
+		return nil, sdkerrors.Wrap(sdkerrors.ErrorInvalidSigner, "validator not in acitve set")
+	}
+
+	// Add the claim to the store
+	att, err := k.AddClaim(ctx, msg.GetType(), msg.GetEventNonce(), validator, msg)
+	if err != nil {
+		return nil, sdkerrors.Wrap(err, "create attestation")
+	}
+
+	// Emit the handle message event
+	ctx.EventManager().EmitEvent(
+		sdk.NewEvent(
+			sdk.EventTypeMessage,
+			sdk.NewAttribute(sdk.AttributeKeyModule, msg.Type()),
+			// TODO: maybe return something better here? is this the right string representation?
+			sdk.NewAttribute(types.AttributeKeyAttestationID, string(types.GetAttestationKey(att.EventNonce, msg))),
+		),
+	)
+
+	return &types.MsgWithdrawClaimResponse{}, nil
 }
+
+// func findValidatorKey(ctx sdk.Context, orchAddr sdk.AccAddress) sdk.ValAddress {
+// 	// todo: implement proper in keeper
+// 	// TODO: do we want ValAddress or do we want the AccAddress for the validator?
+// 	// this is a v important question for encoding
+// 	return sdk.ValAddress(orchAddr)
+// }
