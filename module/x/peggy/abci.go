085e0f9ab878956456a0bfa570563504c0e576f6
Jack Zampolin <jack.zampolin@gmail.com>
2020-12-31 08:33:16 -0800

    Address @fedekunze PR comments

--- module/x/peggy/abci.go
+++ module/x/peggy/abci.go
@@ -4,7 +4,6 @@ import (
 	"github.com/althea-net/peggy/module/x/peggy/keeper"
 	"github.com/althea-net/peggy/module/x/peggy/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 )
 
 // EndBlocker is called at the end of every block
@@ -19,15 +18,15 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 	}
 
 	for i, vs := range valsets {
+		signedWithinWindow := uint64(ctx.BlockHeight())-params.SignedValsetsWindow > vs.Height
 		switch {
 		// #1 condition
 		// We look through the full bonded validator set (not just the active set, include unbonding validators)
 		// and we slash users who haven't signed a valset that is currentHeight - signedBlocksWindow old
-		case uint64(ctx.BlockHeight())-params.SignedValsetsWindow > vs.Height:
+		case signedWithinWindow:
 
 			// first we need to see which validators in the active set
 			// haven't signed the valdiator set and slash them,
-			var toSlash []stakingtypes.Validator
 			confirms := k.GetValsetConfirms(ctx, vs.Nonce)
 			for _, val := range currentBondedSet {
 				found := false
@@ -38,14 +37,12 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 					}
 				}
 				if !found {
-					toSlash = append(toSlash, val)
+					cons, _ := val.GetConsAddr()
+					k.StakingKeeper.Slash(ctx, cons,
+						ctx.BlockHeight(), val.ConsensusPower(),
+						params.SlashFractionValset)
+					k.StakingKeeper.Jail(ctx, cons)
 				}
-			}
-
-			for _, val := range toSlash {
-				cons, _ := val.GetConsAddr()
-				k.StakingKeeper.Slash(ctx, cons, ctx.BlockHeight(), val.ConsensusPower(), params.SlashFractionValset)
-				k.StakingKeeper.Jail(ctx, cons)
 			}
 
 			// then we prune the valset from state
@@ -65,8 +62,8 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 	// and we slash users who haven't signed a batch confirmation that is >15hrs in blocks old
 	batches := k.GetOutgoingTxBatches(ctx)
 	for _, batch := range batches {
-		if uint64(ctx.BlockHeight())-params.SignedBatchesWindow > batch.Block {
-			var toSlash []stakingtypes.Validator
+		signedWithinWindow := uint64(ctx.BlockHeight())-params.SignedBatchesWindow > batch.Block
+		if signedWithinWindow {
 			confirms := k.GetBatchConfirmByNonceAndTokenContract(ctx, batch.BatchNonce, batch.TokenContract)
 			for _, val := range currentBondedSet {
 				found := false
@@ -75,17 +72,14 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 					confVal, _ := sdk.AccAddressFromBech32(conf.Orchestrator)
 					if confVal.Equals(val.GetOperator()) {
 						found = true
+						break
 					}
 				}
 				if !found {
-					toSlash = append(toSlash, val)
+					cons, _ := val.GetConsAddr()
+					k.StakingKeeper.Slash(ctx, cons, ctx.BlockHeight(), val.ConsensusPower(), params.SlashFractionBatch)
+					k.StakingKeeper.Jail(ctx, cons)
 				}
-			}
-			for _, val := range toSlash {
-				cons, _ := val.GetConsAddr()
-				// TODO: make this a different slash fraction in the params
-				k.StakingKeeper.Slash(ctx, cons, ctx.BlockHeight(), val.ConsensusPower(), params.SlashFractionBatch)
-				k.StakingKeeper.Jail(ctx, cons)
 			}
 
 			// clean up batches here
@@ -99,10 +93,7 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 	for _, atts := range attmap {
 		// Conflicting votes should be slashed
 		if len(atts) > 1 {
-			var (
-				toSlash []string
-				unObs   []types.Attestation
-			)
+			var unObs []types.Attestation
 			oneObserved := false
 			for _, att := range atts {
 				if att.Observed == true {
@@ -113,16 +104,16 @@ func EndBlocker(ctx sdk.Context, k keeper.Keeper) {
 			}
 			if oneObserved {
 				for _, att := range unObs {
-					toSlash = append(toSlash, att.Votes...)
+					// toSlash = append(toSlash, att.Votes...)
+					for _, valaddr := range att.Votes {
+						validator, _ := sdk.ValAddressFromBech32(valaddr)
+						val := k.StakingKeeper.Validator(ctx, validator)
+						cons, _ := val.GetConsAddr()
+						k.StakingKeeper.Slash(ctx, cons, ctx.BlockHeight(), k.StakingKeeper.GetLastValidatorPower(ctx, validator), params.SlashFractionConflictingClaim)
+						k.StakingKeeper.Jail(ctx, cons)
+					}
 					k.DeleteAttestation(ctx, att)
 				}
-			}
-			for _, valaddr := range toSlash {
-				validator, _ := sdk.ValAddressFromBech32(valaddr)
-				val := k.StakingKeeper.Validator(ctx, validator)
-				cons, _ := val.GetConsAddr()
-				k.StakingKeeper.Slash(ctx, cons, ctx.BlockHeight(), k.StakingKeeper.GetLastValidatorPower(ctx, validator), params.SlashFractionConflictingClaim)
-				k.StakingKeeper.Jail(ctx, cons)
 			}
 		}
 
