610ff2ae707775914bb796e04af4b307a2b52999
Jtremback <hi@jehan.email>
2021-01-25 13:59:04 -0800

    clean up tests, discrepancy mostly gone

--- solidity/test/submitBatch-vs-logicCall.ts
+++ solidity/test/submitBatch-vs-logicCall.ts
@@ -10,14 +10,44 @@ import {
   signHash,
   examplePowers
 } from "../test-utils/pure";
+import { Signer } from "ethers";
+import { Peggy } from "../typechain/Peggy";
+import { TestERC20A } from "../typechain/TestERC20A";
 
 chai.use(solidity);
 const { expect } = chai;
 
-async function runTest(opts: {
-  batchSize: number
-}) {
+async function prepareTxBatch(batchSize: number, signers: Signer[]) {
+    const numTxs = batchSize;
+    const destinations = new Array(numTxs);
+    const fees = new Array(numTxs);
+    const amounts = new Array(numTxs);
+    for (let i = 0; i < numTxs; i++) {
+      fees[i] = 1;
+      amounts[i] = 1;
+      destinations[i] = await signers[i + 5].getAddress();
+    }
 
+    return {
+      numTxs,
+      destinations,
+      fees,
+      amounts
+    }
+}
+
+async function sendToCosmos(peggy: Peggy, testERC20: TestERC20A, numCoins: number) {
+    // Transfer out to Cosmos, locking coins
+    // =====================================
+    await testERC20.functions.approve(peggy.address, numCoins);
+    await peggy.functions.sendToCosmos(
+      testERC20.address,
+      ethers.utils.formatBytes32String("myCosmosAddress"),
+      numCoins
+    );
+}
+
+async function prep() {
     // Deploy contracts
     // ================
 
@@ -32,39 +62,52 @@ async function runTest(opts: {
     const {
       peggy,
       testERC20,
-      checkpoint: deployCheckpoint
     } = await deployContracts(peggyId, validators, powers, powerThreshold);
 
-    const TestTokenBatchMiddleware = await ethers.getContractFactory("TestTokenBatchMiddleware");
-    const tokenBatchMiddleware = (await TestTokenBatchMiddleware.deploy()) as TestTokenBatchMiddleware;
-    await tokenBatchMiddleware.transferOwnership(peggy.address);
-
-    // Transfer out to Cosmos, locking coins
-    // =====================================
-    await testERC20.functions.approve(peggy.address, 1000);
-    await peggy.functions.sendToCosmos(
-      testERC20.address,
-      ethers.utils.formatBytes32String("myCosmosAddress"),
-      1000
-    );
-
-
-
-
-    // Preparing tx batch
-    // ===================================
-    const numTxs = opts.batchSize;
-    const txDestinationsInt = new Array(numTxs);
-    const txFees = new Array(numTxs);
-    const txAmounts = new Array(numTxs);
-    for (let i = 0; i < numTxs; i++) {
-      txFees[i] = 1;
-      txAmounts[i] = 1;
-      txDestinationsInt[i] = signers[i + 5];
+    return {
+      signers,
+      peggyId,
+      powers,
+      validators,
+      peggy,
+      testERC20,
     }
+}
 
-    const txDestinations = await getSignerAddresses(txDestinationsInt);
+async function runSubmitBatchTest(opts: {
+  batchSize: number
+}) {
+    const {
+      signers,
+      peggyId,
+      powers,
+      validators,
+      peggy,
+      testERC20,
+    } = await prep()
 
+
+
+    // Lock tokens in peggy
+    // ====================
+    await sendToCosmos(peggy, testERC20, 1000)
+
+    expect(
+      (await testERC20.functions.balanceOf(peggy.address)).toNumber(),
+      "peggy does not have correct balance after sendToCosmos"
+    ).to.equal(1000);
+
+    expect(
+      (await testERC20.functions.balanceOf(await signers[0].getAddress())).toNumber(),
+      "msg.sender does not have correct balance after sendToCosmos"
+    ).to.equal(9000);
+
+
+
+
+    // Prepare tx batch
+    // ================
+    const txBatch = await prepareTxBatch(opts.batchSize, signers)
     const batchNonce = 1
 
 
@@ -89,9 +132,9 @@ async function runTest(opts: {
         [
           peggyId,
           methodName,
-          txAmounts,
-          txDestinations,
-          txFees,
+          txBatch.amounts,
+          txBatch.destinations,
+          txBatch.fees,
           batchNonce,
           testERC20.address
         ]
@@ -108,37 +151,97 @@ async function runTest(opts: {
       sigs.r,
       sigs.s,
 
-      txAmounts,
-      txDestinations,
-      txFees,
+      txBatch.amounts,
+      txBatch.destinations,
+      txBatch.fees,
       1,
       testERC20.address
     );
 
     expect(
-      (await testERC20.functions.balanceOf(await signers[6].getAddress())).toNumber()
+      (await testERC20.functions.balanceOf(await signers[5].getAddress())).toNumber(),
+      "first address in tx batch does not have correct balance after submitBatch"
     ).to.equal(1);
+
+    expect(
+      (await testERC20.functions.balanceOf(await signers[5 + txBatch.numTxs - 1].getAddress())).toNumber(),
+      "last address in tx batch does not have correct balance after submitBatch"
+    ).to.equal(1);
+
+    expect(
+      (await testERC20.functions.balanceOf(peggy.address)).toNumber(),
+      "peggy does not have correct balance after submitBatch"
+      // Each tx in batch is worth 1 coin sent + 1 coin fee
+    ).to.equal(1000 - (txBatch.numTxs * 2));
+
+    expect(
+      (await testERC20.functions.balanceOf(await signers[0].getAddress())).toNumber(),
+      "msg.sender does not have correct balance after submitBatch"
+      // msg.sender has received 1 coin in fees for each tx
+    ).to.equal(9000 + txBatch.numTxs);
+}
+
+async function runLogicCallTest(opts: {
+  batchSize: number
+}) {
+    const {
+      signers,
+      peggyId,
+      powers,
+      validators,
+      peggy,
+      testERC20,
+    } = await prep()
+
+    const TestTokenBatchMiddleware = await ethers.getContractFactory("TestTokenBatchMiddleware");
+    const tokenBatchMiddleware = (await TestTokenBatchMiddleware.deploy()) as TestTokenBatchMiddleware;
+    await tokenBatchMiddleware.transferOwnership(peggy.address);
+
+
+    // Lock tokens in peggy
+    // ====================
+    await sendToCosmos(peggy, testERC20, 1000)
+
+    expect(
+      (await testERC20.functions.balanceOf(peggy.address)).toNumber(),
+      "peggy does not have correct balance after sendToCosmos"
+    ).to.equal(1000);
+
+    expect(
+      (await testERC20.functions.balanceOf(await signers[0].getAddress())).toNumber(),
+      "msg.sender does not have correct balance after sendToCosmos"
+    ).to.equal(9000);
+
+
+
+
+    // Preparing tx batch
+    // ===================================
+    const txBatch = await prepareTxBatch(opts.batchSize, signers)
+    const batchNonce = 1
+
+
 
 
     // Using logicCall method
     // ========================
-    methodName = ethers.utils.formatBytes32String(
+    const methodName = ethers.utils.formatBytes32String(
         "logicCall"
       );
 
     let logicCallArgs = {
-      transferAmounts: [100], // transferAmounts
+      transferAmounts: [txBatch.numTxs], // transferAmounts
       transferTokenContracts: [testERC20.address], // transferTokenContracts
-      feeAmounts: [100], // feeAmounts
+      feeAmounts: [txBatch.numTxs], // feeAmounts
       feeTokenContracts: [testERC20.address], // feeTokenContracts
       logicContractAddress: tokenBatchMiddleware.address, // logicContractAddress
-      payload: tokenBatchMiddleware.interface.functions.submitBatch.encode([txAmounts, txDestinations, testERC20.address]), // payload
+      payload: tokenBatchMiddleware.interface.functions.submitBatch.encode([txBatch.amounts, txBatch.destinations, testERC20.address]), // payload
       timeOut: 4766922941000, // timeOut, Far in the future
       invalidationId: ethers.utils.hexZeroPad(testERC20.address, 32), // invalidationId
       invalidationNonce: 1 // invalidationNonce
     }
   
-    digest = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(
+    const digest = ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(
           [
             "bytes32", // peggyId
             "bytes32", // methodName
@@ -167,7 +270,7 @@ async function runTest(opts: {
           ]
         ));
   
-      sigs = await signHash(validators, digest);
+      const sigs = await signHash(validators, digest);
   
       await peggy.submitLogicCall(
         await getSignerAddresses(validators),
@@ -181,17 +284,36 @@ async function runTest(opts: {
       );
 
       expect(
-        (await testERC20.functions.balanceOf(await signers[6].getAddress())).toNumber()
-      ).to.equal(2);
-
+        (await testERC20.functions.balanceOf(await signers[5].getAddress())).toNumber(),
+        "first address in tx batch does not have correct balance after submitLogicCall"
+      ).to.equal(1);
+  
+      expect(
+        (await testERC20.functions.balanceOf(await signers[5 + txBatch.numTxs - 1].getAddress())).toNumber(),
+        "last address in tx batch does not have correct balance after submitLogicCall"
+      ).to.equal(1);
+  
+      expect(
+        (await testERC20.functions.balanceOf(peggy.address)).toNumber(),
+        "peggy does not have correct balance after submitLogicCall"
+        // Each tx in batch is worth 1 coin sent + 1 coin fee
+      ).to.equal(1000 - (txBatch.numTxs * 2));
+  
+      expect(
+        (await testERC20.functions.balanceOf(await signers[0].getAddress())).toNumber(),
+        "msg.sender does not have correct balance after submitLogicCall"
+        // msg.sender has received 1 coin in fees for each tx
+      ).to.equal(9000 + txBatch.numTxs);
 }
 
 describe.only("Compare gas usage of old submitBatch method vs new logicCall method submitting one batch", function () {
   it("Large batch", async function () {
-    await runTest({batchSize: 100})
+    await runSubmitBatchTest({batchSize: 100})
+    await runLogicCallTest({batchSize: 100})
   });
 
   it("Small batch", async function () {
-    await runTest({batchSize: 10})
+    await runSubmitBatchTest({batchSize: 1})
+    await runLogicCallTest({batchSize: 1})
   });
 });
