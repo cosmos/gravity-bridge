477bb608915fda18739f3a8f86320aad146a5987
Jtremback <hi@jehan.email>
2021-01-27 17:23:52 -0800

    more clarification

--- module/x/peggy/abci.go
+++ module/x/peggy/abci.go
@@ -189,23 +189,28 @@ func attestationTally(ctx sdk.Context, k keeper.Keeper) {
 	// This iterates over all keys (event nonces) in the attestation mapping. Each value contains
 	// a slice with one or more attestations at that event nonce. There can be multiple attestations
 	// at one event nonce when validators disagree about what event happened at that nonce.
-	for _, key := range keys {
+	for _, nonce := range keys {
 		// This iterates over all attestations at a particular event nonce.
 		// They are ordered by when the first attestation at the event nonce was received.
 		// This order is not important.
-		for _, att := range attmap[key] {
+		for _, att := range attmap[nonce] {
 			// We check if the event nonce is exactly 1 higher than the last attestation that was
 			// observed. If it is not, we just move on to the next nonce. This will skip over all
-			// attestations that have already been observed. Once an attestation at a given event
-			// nonce has enough votes and becomes observed, every other attestation at that nonce
-			// will be skipped, since the lastObservedEventNonce will be incremented.
+			// attestations that have already been observed.
 			//
-			// Then we go to the next event nonce in the attestation mapping, if there is one.
+			// Once we hit an event nonce that is one higher than the last observed event, we stop
+			// skipping over this conditional and start calling tryAttestation (counting votes)
+			// Once an attestation at a given event nonce has enough votes and becomes observed,
+			// every other attestation at that nonce will be skipped, since the lastObservedEventNonce
+			// will be incremented.
+			//
+			// Then we go to the next event nonce in the attestation mapping, if there is one. This
+			// nonce will once again be one higher than the lastObservedEventNonce.
 			// If there is an attestation at this event nonce which has enough votes to be observed,
 			// we skip the other attestations and move on to the next nonce again.
 			// If no attestation becomes observed, when we get to the next nonce, every attestation in
 			// it will be skipped. The same will happen for every nonce after that.
-			if key == uint64(k.GetLastObservedEventNonce(ctx))+1 {
+			if nonce == uint64(k.GetLastObservedEventNonce(ctx))+1 {
 				k.TryAttestation(ctx, &att)
 			}
 		}
