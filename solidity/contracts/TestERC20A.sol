b4f229ce8be48f8c7dc543c169c89f04db276e02
Justin Kilpatrick <justin@althea.net>
2021-02-13 06:41:37 -0500

    Deploy more ERC20 tokens in tests
    10k of each ERC20 is not actually that many if we intended to test
    with whole coins.

--- solidity/contracts/TestERC20A.sol
+++ solidity/contracts/TestERC20A.sol
@@ -12,6 +12,6 @@ contract TestERC20A is ERC20 {
 		// this is the EtherBase address for our testnet miner in
 		// tests/assets/ETHGenesis.json so it wil have both a lot
 		// of ETH and a lot of erc20 tokens to test with
-		_mint(0xBf660843528035a5A4921534E156a27e64B231fE, 10000000000000000000000);
+		_mint(0xBf660843528035a5A4921534E156a27e64B231fE, 100000000000000000000000000);
 	}
 }
