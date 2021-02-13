a8e835c13c3190ea597f3abc8b40cbc3f48b6332
Justin Kilpatrick <justin@althea.net>
2021-02-12 21:08:18 -0500

    Update contract deployer script example
    Several of these arguments are no longer used.

--- solidity/scripts/contract-deployer.sh
+++ solidity/scripts/contract-deployer.sh
@@ -4,7 +4,5 @@ contract-deployer.ts \
 --cosmos-node="http://localhost:26657" \
 --eth-node="http://localhost:8545" \
 --eth-privkey="0xb1bab011e03a9862664706fc3bbaa1b16651528e5f0e7fbfcbfdd8be302a13e7" \
---peggy-id="defaultpeggyid" \
---contract=artifacts/Peggy.json \
---erc20-contract=artifacts/TestERC20.json \
+--contract=Peggy.json \
 --test-mode=true
