d208b795b1a90bbad47d5b0a58042954ef29ed27
Justin Kilpatrick <justin@althea.net>
2021-03-03 15:14:56 -0500

    harden wait_for_next_cosmos_block
    This should never crash because of bad endpoint input, it should only
    panic once the timeout has elapsed

--- orchestrator/cosmos_peggy/src/utils.rs
+++ orchestrator/cosmos_peggy/src/utils.rs
@@ -5,42 +5,24 @@ use tokio::time::delay_for;
 
 pub async fn wait_for_next_cosmos_block(contact: &Contact, timeout: Duration) {
     let start = Instant::now();
-    let current_block = contact
-        .get_latest_block()
-        .await
-        .unwrap()
-        .block
-        .unwrap()
-        .last_commit
-        .height;
-    while current_block
-        == contact
-            .get_latest_block()
-            .await
-            .unwrap()
-            .block
-            .unwrap()
-            .last_commit
-            .height
-    {
-        delay_for(Duration::from_secs(1)).await;
-        if Instant::now() - start > timeout {
-            panic!("Cosmos chain took too long to produce a block!");
+    let mut last_height = None;
+    while Instant::now() - start < timeout {
+        if let Ok(block_response) = contact.get_latest_block().await {
+            if let Some(block) = block_response.block {
+                if last_height.is_some() && block.last_commit.height > last_height.unwrap() {
+                    return;
+                }
+                last_height = Some(block.last_commit.height);
+            }
         }
+        delay_for(Duration::from_secs(1)).await;
     }
+    panic!("Cosmos chain took too long to produce a block!");
 }
 
 pub async fn wait_for_cosmos_online(contact: &Contact, timeout: Duration) {
-    let start = Instant::now();
-    let mut current_block = contact.get_latest_block().await;
-    while current_block.is_err() {
-        delay_for(Duration::from_secs(1)).await;
-        current_block = contact.get_latest_block().await;
-        if Instant::now() - start > timeout {
-            panic!("Cosmos chain took too long to start!");
-        }
-    }
     // we have a block now, wait for a few more.
+    wait_for_next_cosmos_block(contact, timeout).await;
     wait_for_next_cosmos_block(contact, timeout).await;
     wait_for_next_cosmos_block(contact, timeout).await;
 }
