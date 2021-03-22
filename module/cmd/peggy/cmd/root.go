8d265c94fea75834901b72e7ba84a6f19f30007e
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-03-21 17:20:24 -0700

    add ethereum key generation to tool

--- module/cmd/peggy/cmd/root.go
+++ module/cmd/peggy/cmd/root.go
@@ -108,6 +108,7 @@ func initRootCmd(rootCmd *cobra.Command, encodingConfig params.EncodingConfig) {
 		queryCommand(),
 		txCommand(),
 		keys.Commands(app.DefaultNodeHome),
+		Commands(app.DefaultNodeHome),
 	)
 }
 
