1e26e078d534c203cf9484334c3bf2a2d1fd974c
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-03-26 08:56:16 -0700

    working env vars

--- orchestrator/startup.sh
+++ orchestrator/startup.sh
@@ -1,8 +1,8 @@
 cargo run --release orchestrator \
-    --cosmos-key=${COSMOS_KEY} \
-    --ethereum-key=${ETH_PRIVATE_KEY} \
-    --cosmos-legacy-rpc=${COSMOS_RPC} \
-    --cosmos-grpc=${COSMOS_GRPC} \
-    --ethereum-rpc=${ETH_RPC} \
-    --fees=${DENOM} \
-    --contract-address=${CONTRACT_ADDR}
+    --cosmos-phrase="\"${COSMOS_PHRASE}\"" \
+    --ethereum-key="\"${ETH_PRIVATE_KEY}\"" \
+    --cosmos-legacy-rpc="\"${COSMOS_RPC}\"" \
+    --cosmos-grpc="\"${COSMOS_GRPC}\"" \
+    --ethereum-rpc="\"${ETH_RPC}\"" \
+    --fees="${DENOM}" \
+    --contract-address="\"${CONTRACT_ADDR}\""
