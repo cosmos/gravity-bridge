f35117e1b714f830a71fbf682768dc00c345bc34
Justin Kilpatrick <justin@althea.net>
2021-04-15 15:51:14 -0400

    Add script for running all integration tests
    This is just an easy way to run all the integration tests locally

---
+++ tests/run-all-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eux
+# the directory of this script, useful for allowing this script
+# to be run with any PWD
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+bash all-up-test.sh
+bash all-up-test.sh VALSET_STRESS
+bash all-up-test.sh BATCH_STRESS
+bash all-up-test.sh HAPPY_PATH_V2
+bash all-up-test.sh VALIDATOR_OUT
+bash all-up-test.sh ARBITRARY_LOGIC
