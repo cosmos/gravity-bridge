9d189985edb8652c99c15b29c4aae9d91b293fbf
kobs30 <sergeik30@gmail.com>
2021-04-15 22:47:27 +0200

    Update run-testnet.sh
    Add pprof server config

--- tests/container-scripts/run-testnet.sh
+++ tests/container-scripts/run-testnet.sh
@@ -34,8 +34,9 @@ GRPC_ADDRESS="--grpc.address 7.7.7.$i:9091"
 fi
 LISTEN_ADDRESS="--address tcp://7.7.7.$i:26655"
 P2P_ADDRESS="--p2p.laddr tcp://7.7.7.$i:26656"
+PPROF_ADDRESS="--rpc.pprof_laddr tcp://7.7.7.$i:6060"
 LOG_LEVEL="--log_level error"
-ARGS="$GAIA_HOME $LISTEN_ADDRESS $RPC_ADDRESS $GRPC_ADDRESS $LOG_LEVEL $P2P_ADDRESS"
+ARGS="$GAIA_HOME $LISTEN_ADDRESS $RPC_ADDRESS $GRPC_ADDRESS $LOG_LEVEL $P2P_ADDRESS $PPROF_ADDRESS"
 $BIN $ARGS start > /validator$i/logs &
 done
 
