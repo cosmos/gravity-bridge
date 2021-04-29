a774e9b15aecb0acfc6484db78265f1aa1c40b61
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-04-29 15:33:25 -0400

    remove unused arg

--- orchestrator/startup.sh
+++ orchestrator/startup.sh
@@ -1,14 +1,12 @@
 #bin/sh
 
 validator_address=$(getent hosts ${VALIDATOR} | awk '{ print $1 }')
-rpc="http://$validator_address:1317"
 grpc="http://$validator_address:9090"
 ethrpc="http://$(getent hosts ethereum | awk '{ print $1 }'):8545"
 
 echo cargo run --bin orchestrator -- \
     --cosmos-phrase="${COSMOS_PHRASE}" \
     --ethereum-key="${ETH_PRIVATE_KEY}" \
-    --cosmos-legacy-rpc="$rpc" \
     --cosmos-grpc="$grpc" \
     --ethereum-rpc="$ethrpc" \
     --fees="${DENOM}" \
@@ -17,7 +15,6 @@ echo cargo run --bin orchestrator -- \
 cargo run --bin orchestrator -- \
     --cosmos-phrase="${COSMOS_PHRASE}" \
     --ethereum-key="${ETH_PRIVATE_KEY}" \
-    --cosmos-legacy-rpc="$rpc" \
     --cosmos-grpc="$grpc" \
     --ethereum-rpc="$ethrpc" \
     --fees="${DENOM}" \
