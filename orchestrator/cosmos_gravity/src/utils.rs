0725a628bb315e93eefcb5bcb41986a42161445a
Justin Kilpatrick <justin@althea.net>
2021-05-21 18:51:06 -0400

    Better handle slow node start during tests
    This has caused some test failures both downstream and upstream,
    hopefully this simple check will make things more stable.

--- orchestrator/cosmos_gravity/src/utils.rs
+++ orchestrator/cosmos_gravity/src/utils.rs
@@ -1,8 +1,16 @@
+use deep_space::error::CosmosGrpcError;
 use deep_space::Contact;
-use std::time::Duration;
+use std::time::{Duration, Instant};
+use tokio::time::sleep;
 
 pub async fn wait_for_cosmos_online(contact: &Contact, timeout: Duration) {
-    // we have a block now, wait for a few more.
+    let start = Instant::now();
+    while let Err(CosmosGrpcError::NodeNotSynced) = contact.wait_for_next_block(timeout).await {
+        sleep(Duration::from_secs(1)).await;
+        if Instant::now() - start > timeout {
+            panic!("Cosmos node has not come online during timeout!")
+        }
+    }
     contact.wait_for_next_block(timeout).await.unwrap();
     contact.wait_for_next_block(timeout).await.unwrap();
     contact.wait_for_next_block(timeout).await.unwrap();
