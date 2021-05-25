7fe37100f289371e489e123e715ea37b88ce99bd
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-05-24 17:52:56 -0700

    logging and direct container call

--- orchestrator/test_runner/startup.sh
+++ orchestrator/test_runner/startup.sh
@@ -1,6 +1,7 @@
-validator_address=$(getent hosts ${VALIDATOR} | awk '{ print $1 }')
+validator_address=$(getent hosts gravity0 | awk '{ print $1 }')
 abci="http://$validator_address:26657"
 grpc="http://$validator_address:9090"
 ethrpc="http://$(getent hosts ethereum | awk '{ print $1 }'):8545"
 
+echo COSMOS_NODE_GRPC="$grpc" COSMOS_NODE_ABCI="$abci" ETH_NODE="$ethrpc" RUST_BACKTRACE=full TEST_TYPE="VALSET_STRESS" RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --bin test_runner
 COSMOS_NODE_GRPC="$grpc" COSMOS_NODE_ABCI="$abci" ETH_NODE="$ethrpc" RUST_BACKTRACE=full TEST_TYPE="VALSET_STRESS" RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --bin test_runner
