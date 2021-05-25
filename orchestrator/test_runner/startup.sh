44a696a3c3a2ab1b378bf82017cc422c94f44699
Jehan Tremback <jehan.tremback@gmail.com>
2021-05-25 00:14:02 +0000

    improve rust build and caching

--- orchestrator/test_runner/startup.sh
+++ orchestrator/test_runner/startup.sh
@@ -3,4 +3,4 @@ abci="http://$validator_address:26657"
 grpc="http://$validator_address:9090"
 ethrpc="http://$(getent hosts ethereum | awk '{ print $1 }'):8545"
 
-COSMOS_NODE_GRPC="$grpc" COSMOS_NODE_ABCI="$abci" ETH_NODE="$ethrpc" RUST_BACKTRACE=full TEST_TYPE="VALSET_STRESS" RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --release --bin test_runner
+COSMOS_NODE_GRPC="$grpc" COSMOS_NODE_ABCI="$abci" ETH_NODE="$ethrpc" RUST_BACKTRACE=full TEST_TYPE="VALSET_STRESS" RUST_LOG=INFO PATH=$PATH:$HOME/.cargo/bin cargo run --bin test_runner
