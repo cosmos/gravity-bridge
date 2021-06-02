9d7868c2a43699400d04ea52900ffb70e49edad9
Justin Kilpatrick <justin@althea.net>
2021-06-02 18:02:45 -0400

    Handle chain waiting to start for tests
    The previous patch includes new code from deep_space which changes the
    response on this line.

--- orchestrator/cosmos_gravity/src/utils.rs
+++ orchestrator/cosmos_gravity/src/utils.rs
@@ -5,7 +5,9 @@ use tokio::time::sleep;
 
 pub async fn wait_for_cosmos_online(contact: &Contact, timeout: Duration) {
     let start = Instant::now();
-    while let Err(CosmosGrpcError::NodeNotSynced) = contact.wait_for_next_block(timeout).await {
+    while let Err(CosmosGrpcError::NodeNotSynced) | Err(CosmosGrpcError::ChainNotRunning) =
+        contact.wait_for_next_block(timeout).await
+    {
         sleep(Duration::from_secs(1)).await;
         if Instant::now() - start > timeout {
             panic!("Cosmos node has not come online during timeout!")
