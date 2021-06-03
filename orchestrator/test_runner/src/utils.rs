2f46b6c30a942ac3f157237f6997f11c939be70a
Justin Kilpatrick <justin@althea.net>
2021-06-03 09:31:32 -0400

    De-duplicate eth sending in tests
    Previously we had two functions to send Ethereum in parallel, this patch
    de-duplicates those functions and uses a general purpose send_eth_bulk
    to replace the highly specific send_eth_to_orchestrators parallel send
    implementation that was mostly identical anyways.

--- orchestrator/test_runner/src/utils.rs
+++ orchestrator/test_runner/src/utils.rs
@@ -19,73 +19,21 @@ use rand::Rng;
 use std::thread;
 use web30::{client::Web3, types::SendTxOption};
 
-/// This overly complex function primarily exists to parallelize the sending of Eth to the
-/// orchestrators, waiting for these there transactions takes up nearly a minute of test time
-/// and it seemed like low hanging fruit. It in fact was not, mostly because we are sending
-/// these tx's from the same address and we therefore need to take into account the correct
-/// nonce given the other transactions in flight. This means we need to build the transactions
-/// ourselves with that info right here. If you have to modify this seriously consider
-/// just calling send_one_eth in a loop.
 pub async fn send_eth_to_orchestrators(keys: &[ValidatorKeys], web30: &Web3) {
     let balance = web30.eth_get_balance(*MINER_ADDRESS).await.unwrap();
     info!(
         "Sending orchestrators 100 eth to pay for fees miner has {} ETH",
         balance / one_eth()
     );
-    let mut eth_addresses = Vec::new();
-    for k in keys {
-        let e_key = k.eth_key;
-        eth_addresses.push(e_key.to_public_key().unwrap())
+    let mut eth_keys = Vec::new();
+    for key in keys {
+        eth_keys.push(key.eth_key.to_public_key().unwrap());
     }
-    let net_version = web30.net_version().await.unwrap();
-    let mut nonce = web30
-        .eth_get_transaction_count(*MINER_ADDRESS)
-        .await
-        .unwrap();
-    let mut transactions = Vec::new();
-    for address in eth_addresses {
-        let t = Transaction {
-            to: address,
-            nonce: nonce.clone(),
-            gas_price: 1_000_000_000u64.into(),
-            gas_limit: 24000u64.into(),
-            value: one_eth() * 100u16.into(),
-            data: Vec::new(),
-            signature: None,
-        };
-        let t = t.sign(&*MINER_PRIVATE_KEY, Some(net_version));
-        transactions.push(t);
-        nonce += 1u64.into();
-    }
-    let mut sends = Vec::new();
-    for tx in transactions {
-        sends.push(web30.eth_send_raw_transaction(tx.to_bytes().unwrap()));
-    }
-    let txids = join_all(sends).await;
-    let mut wait_for_txid = Vec::new();
-    for txid in txids {
-        let wait = web30.wait_for_transaction(txid.unwrap(), TOTAL_TIMEOUT, None);
-        wait_for_txid.push(wait);
-    }
-    join_all(wait_for_txid).await;
+    send_eth_bulk(one_eth() * 100u16.into(), &eth_keys, web30).await;
 }
 
 pub async fn send_one_eth(dest: EthAddress, web30: &Web3) {
-    let txid = web30
-        .send_transaction(
-            dest,
-            Vec::new(),
-            1_000_000_000_000_000_000u128.into(),
-            *MINER_ADDRESS,
-            *MINER_PRIVATE_KEY,
-            vec![],
-        )
-        .await
-        .expect("Failed to send Eth to validator {}");
-    web30
-        .wait_for_transaction(txid, TOTAL_TIMEOUT, None)
-        .await
-        .unwrap();
+    send_eth_bulk(one_eth(), &[dest], web30).await;
 }
 
 pub async fn check_cosmos_balance(
