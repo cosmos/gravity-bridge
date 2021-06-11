7a82310e8ed2a473236ca1056389d305a0bbbab3
Justin Kilpatrick <justin@althea.net>
2021-06-11 09:27:26 -0400

    Ensure config is updated when new fields are added
    This default pattern will allow us to add new fields and expand the
    existing on disk config effectively without simply crashing becuase the
    old on disk config is missing the new field.

--- orchestrator/gravity_utils/src/types/config.rs
+++ orchestrator/gravity_utils/src/types/config.rs
@@ -3,7 +3,9 @@
 /// Global configuration struct for Gravity bridge tools
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq, Default)]
 pub struct GravityBridgeToolsConfig {
+    #[serde(default = "RelayerConfig::default")]
     pub relayer: RelayerConfig,
+    #[serde(default = "OrchestratorConfig::default")]
     pub orchestrator: OrchestratorConfig,
 }
 
@@ -15,13 +17,18 @@ pub struct RelayerConfig {}
 #[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
 pub struct OrchestratorConfig {
     /// If this Orchestrator should run an integrated relayer or not
+    #[serde(default = "default_relayer_enabled")]
     pub relayer_enabled: bool,
+}
+
+fn default_relayer_enabled() -> bool {
+    true
 }
 
 impl Default for OrchestratorConfig {
     fn default() -> Self {
         OrchestratorConfig {
-            relayer_enabled: true,
+            relayer_enabled: default_relayer_enabled(),
         }
     }
 }
