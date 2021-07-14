8b3af77b160f5e2978f715999b53e19aacfcef60
Hannydevelop <ukpaiugochiibem @gmail.com>
2021-07-14 16:00:43 +0100

    Fixing cosmos show keys

--- orchestrator/gorc/src/commands/keys/cosmos/show.rs
+++ orchestrator/gorc/src/commands/keys/cosmos/show.rs
@@ -1,4 +1,6 @@
 use abscissa_core::{Command, Options, Runnable};
+use signatory::FsKeyStore;
+use std::path::Path;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct ShowCosmosKeyCmd {
@@ -10,5 +12,11 @@ pub struct ShowCosmosKeyCmd {
 impl Runnable for ShowCosmosKeyCmd {
     fn run(&self) {
         // todo(shella): glue with signatory crate to list keys
+        let keystore_path = Path::new("/tmp/keystore");
+        let keystore = FsKeyStore::create_or_open(keystore_path).unwrap();
+        let key_name = &self.name.parse().unwrap();
+        let key_info = keystore.info(&key_name).unwrap();
+        let show_key = FsKeyStore::info(&keystore, &key_name).unwrap();
+        println!("{:?}", show_key)
     }
 }
