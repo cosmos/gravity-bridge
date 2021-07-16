91b781821864fbf8bb8d467bd71980e00ccbfffe
Levi Cook <levicook@gmail.com>
2021-07-16 00:08:33 +0000

    MetaMask compatible!

--- orchestrator/gorc/src/commands/keys/eth/add.rs
+++ orchestrator/gorc/src/commands/keys/eth/add.rs
@@ -4,25 +4,25 @@ use abscissa_core::{Application, Command, Options, Runnable};
 use bip32;
 use k256::pkcs8::ToPrivateKey;
 use rand_core::OsRng;
-use std::path;
+use signatory::FsKeyStore;
+use std::path::Path;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct AddEthKeyCmd {
-    #[options(free, help = "add [name] (password)")]
+    #[options(free, help = "add [name]")]
     pub args: Vec<String>,
 
     #[options(help = "overwrite existing key")]
     pub overwrite: bool,
 }
 
-// Entry point for `gorc keys eth add [name] (password)`
+// Entry point for `gorc keys eth add [name]`
 // - [name] required; key name
-// - (password) optional; when absent the user will be prompted to enter it
 impl Runnable for AddEthKeyCmd {
     fn run(&self) {
         let config = APP.config();
-        let keystore = path::Path::new(&config.keystore);
-        let keystore = signatory::FsKeyStore::create_or_open(keystore).unwrap();
+        let keystore = Path::new(&config.keystore);
+        let keystore = FsKeyStore::create_or_open(keystore).expect("Could not open keystore");
 
         let name = self.args.get(0).expect("name is required");
         let name = name.parse().expect("Could not parse name");
@@ -33,16 +33,11 @@ impl Runnable for AddEthKeyCmd {
             }
         }
 
-        let password = match self.args.get(1) {
-            Some(password) => password.clone(),
-            None => rpassword::read_password_from_tty(Some("Password: ")).unwrap(),
-        };
-
         let mnemonic = bip32::Mnemonic::random(&mut OsRng, Default::default());
         println! {"**Important** record this mnemonic in a safe place:"}
         println! {"{}", mnemonic.phrase()};
 
-        let seed = mnemonic.to_seed(&password);
+        let seed = mnemonic.to_seed("");
 
         let path = config.ethereum.key_derivation_path.trim();
         let path = path
--- orchestrator/gorc/src/commands/keys/eth/import.rs
+++ orchestrator/gorc/src/commands/keys/eth/import.rs
@@ -2,26 +2,26 @@ use super::show::ShowEthKeyCmd;
 use crate::application::APP;
 use abscissa_core::{Application, Command, Options, Runnable};
 use k256::pkcs8::ToPrivateKey;
-use std::path;
+use signatory::FsKeyStore;
+use std::path::Path;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct ImportEthKeyCmd {
-    #[options(free, help = "import [name] (mnemonic) (password)")]
+    #[options(free, help = "import [name] (mnemonic)")]
     pub args: Vec<String>,
 
     #[options(help = "overwrite existing key")]
     pub overwrite: bool,
 }
 
-// Entry point for `gorc keys eth import [name] (mnemonic) (password)`
+// Entry point for `gorc keys eth import [name] (mnemonic)`
 // - [name] required; key name
 // - (mnemonic) optional; when absent the user will be prompted to enter it
-// - (password) optional; when absent the user will be prompted to enter it
 impl Runnable for ImportEthKeyCmd {
     fn run(&self) {
         let config = APP.config();
-        let keystore = path::Path::new(&config.keystore);
-        let keystore = signatory::FsKeyStore::create_or_open(keystore).unwrap();
+        let keystore = Path::new(&config.keystore);
+        let keystore = FsKeyStore::create_or_open(keystore).expect("Could not open keystore");
 
         let name = self.args.get(0).expect("name is required");
         let name = name.parse().expect("Could not parse name");
@@ -34,26 +34,27 @@ impl Runnable for ImportEthKeyCmd {
 
         let mnemonic = match self.args.get(1) {
             Some(mnemonic) => mnemonic.clone(),
-            None => rpassword::read_password_from_tty(Some("Mnemonic: ")).unwrap(),
+            None => rpassword::read_password_from_tty(Some("Mnemonic: "))
+                .expect("Could not read mnemonic"),
         };
 
-        let password = match self.args.get(2) {
-            Some(password) => password.clone(),
-            None => rpassword::read_password_from_tty(Some("Password: ")).unwrap(),
-        };
+        let mnemonic = bip32::Mnemonic::new(mnemonic.trim(), Default::default())
+            .expect("Could not parse mnemonic");
 
-        let mnemonic = bip32::Mnemonic::new(mnemonic.trim(), Default::default()).unwrap();
-
-        let seed = mnemonic.to_seed(&password.trim());
+        let seed = mnemonic.to_seed("");
 
         let path = config.ethereum.key_derivation_path.trim();
         let path = path
             .parse::<bip32::DerivationPath>()
             .expect("Could not parse derivation path");
 
-        let key = bip32::XPrv::derive_from_path(seed, &path).unwrap();
+        let key = bip32::XPrv::derive_from_path(seed, &path).expect("Could not derive key");
         let key = k256::SecretKey::from(key.private_key());
-        let key = key.to_pkcs8_der().unwrap();
+
+        let key = key
+            .to_pkcs8_der()
+            .expect("Could not PKCS8 encod private key");
+
         keystore.store(&name, &key).expect("Could not store key");
 
         let args = vec![name.to_string()];
