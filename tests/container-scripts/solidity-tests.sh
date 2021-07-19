a244e5aa27fa87c1c693b0ebf478adc2b29a74c4
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2021-07-19 10:55:53 -0600

    Delete old test setup scripts (#79)
    * fix proto-check-breaking
    
    * delete old test directory

--- tests/container-scripts/solidity-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Builds and runs Solidity tests within a container
-
-pushd /gravity/solidity/
-rm -rf node_modules
-HUSKY_SKIP_INSTALL=1 npm install
-npm run typechain
-npm run evm &
-npm run test
--- tests/run-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-TEST_TYPE=$1
-set -eu
-
-# Run test entry point script
-docker exec gravity_test_instance /bin/sh -c "pushd /gravity/ && tests/container-scripts/integration-tests.sh 1 $TEST_TYPE"
