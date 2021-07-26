7957a83d76d794cc233b8b9cbe3e62463f3b4a13
Levi Cook <levicook@users.noreply.github.com>
2021-07-26 11:55:11 -0600

    validate validator address & fetch their account sequence for optional nonce (#101)

--- orchestrator/gorc/src/commands/sign_delegate_keys.rs
+++ orchestrator/gorc/src/commands/sign_delegate_keys.rs
@@ -1,11 +1,7 @@
 use crate::{application::APP, prelude::*};
 use abscissa_core::{Application, Command, Options, Runnable};
-use cosmos_gravity::query;
 use gravity_proto::gravity as proto;
-use gravity_utils::connection_prep::create_rpc_connections;
-use orchestrator::main_loop::{ETH_ORACLE_LOOP_SPEED, ETH_SIGNER_LOOP_SPEED};
-use relayer::main_loop::LOOP_SPEED as RELAYER_LOOP_SPEED;
-use std::cmp::min;
+use std::time::Duration;
 
 #[derive(Command, Debug, Default, Options)]
 pub struct SignDelegateKeysCmd {
@@ -26,32 +22,24 @@ impl Runnable for SignDelegateKeysCmd {
             let key = config.load_clarity_key(name.clone());
 
             let val = self.args.get(1).expect("validator-address is required");
-            // TODO(levi) ensure this is a valoper address for the next release
+            let address = val.parse().expect("Could not parse address");
 
-            let cosmos_prefix = config.cosmos.prefix.clone();
-
-            let timeout = min(
-                min(ETH_SIGNER_LOOP_SPEED, ETH_ORACLE_LOOP_SPEED),
-                RELAYER_LOOP_SPEED,
-            );
-
-            let nonce = match self.args.get(2) {
-                Some(nonce) => nonce.clone(),
+            let nonce: u64 = match self.args.get(2) {
+                Some(nonce) => nonce.parse().expect("cannot parse nonce"),
                 None => {
-                    let connections = create_rpc_connections(
-                        cosmos_prefix,
-                        Some(config.cosmos.grpc.clone()),
-                        Some(config.ethereum.rpc.clone()),
+                    let timeout = Duration::from_secs(10);
+                    let contact = deep_space::Contact::new(
+                        &config.cosmos.grpc,
                         timeout,
+                        &config.cosmos.prefix,
                     )
-                    .await;
-                    let mut grpc = connections.grpc.clone().unwrap();
-                    let valset = query::get_latest_valset(&mut grpc).await;
-                    let valset = valset.unwrap().expect("Valset cannot be retrieved");
-                    valset.nonce.to_string()
+                    .expect("Could not create contact");
+
+                    let account_info = contact.get_account_info(address).await;
+                    let account_info = account_info.expect("Did not receive account info");
+                    account_info.sequence
                 }
             };
-            let nonce = nonce.parse::<u64>().expect("cannot parse nonce");
 
             let msg = proto::DelegateKeysSignMsg {
                 validator_address: val.clone(),
