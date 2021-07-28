a3f31254024d0913ed8c648e30daaa566c601b29
Aleksandr Bezobchuk <alexanderbez@users.noreply.github.com>
2021-07-28 11:08:12 -0400

    x/gravity: support tokens without metadata (#104)

--- module/x/gravity/types/errors.go
+++ module/x/gravity/types/errors.go
@@ -5,8 +5,9 @@ import (
 )
 
 var (
-	ErrInvalid        = sdkerrors.Register(ModuleName, 3, "invalid")
-	ErrSupplyOverflow = sdkerrors.Register(ModuleName, 4, "malicious ERC20 with invalid supply sent over bridge")
-	ErrDelegateKeys   = sdkerrors.Register(ModuleName, 5, "failed to delegate keys")
-	ErrEmptyEthSig    = sdkerrors.Register(ModuleName, 6, "empty Ethereum signature")
+	ErrInvalid           = sdkerrors.Register(ModuleName, 3, "invalid")
+	ErrSupplyOverflow    = sdkerrors.Register(ModuleName, 4, "malicious ERC20 with invalid supply sent over bridge")
+	ErrDelegateKeys      = sdkerrors.Register(ModuleName, 5, "failed to delegate keys")
+	ErrEmptyEthSig       = sdkerrors.Register(ModuleName, 6, "empty Ethereum signature")
+	ErrInvalidERC20Event = sdkerrors.Register(ModuleName, 7, "invalid ERC20 deployed event")
 )
