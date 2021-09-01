8af32db2883af08acd35bd76703eac39ab283eaa
Jack Zampolin <jack.zampolin@gmail.com>
2021-09-01 15:45:02 -0700

    Push main @jackzampolin: update to sdk v0.44.0 for security release

--- module/cmd/gravity/cmd/gentx.go
+++ module/cmd/gravity/cmd/gentx.go
@@ -191,14 +191,14 @@ $ %s gentx my-key-name 1000000stake 0x033030FEeBd93E3178487c35A9c8cA80874353C9 c
 
 			if key.GetType() == keyring.TypeOffline || key.GetType() == keyring.TypeMulti {
 				cmd.PrintErrln("Offline key passed in. Use `tx sign` command to sign.")
-				return authclient.PrintUnsignedStdTx(txBldr, clientCtx, msgs)
+				return txBldr.PrintUnsignedTx(clientCtx, msgs...)
 			}
 
 			// write the unsigned transaction to the buffer
 			w := bytes.NewBuffer([]byte{})
 			clientCtx = clientCtx.WithOutput(w)
 
-			if err = authclient.PrintUnsignedStdTx(txBldr, clientCtx, msgs); err != nil {
+			if err = txBldr.PrintUnsignedTx(clientCtx, msgs...); err != nil {
 				return errors.Wrap(err, "failed to print unsigned std tx")
 			}
 
