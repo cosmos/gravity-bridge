06871a9e967f067c5eafe9ab7cdf7db7b86d3a6c
hannydevelop <ukpaiugochiibem@gmail.com>
2021-09-05 12:15:38 +0000

    making cargo.toml non virtual

--- orchestrator/src/lib.rs
+++ orchestrator/src/lib.rs
@@ -1,2 +1,11 @@
 pub use orchestrator as orchestrator;
 pub use relayer as relayer;
+pub use cosmos_gravity as cosmos_gravity;
+pub use ethereum_gravity as ethereum_gravity;
+pub use gravity_utils as gravity_utils;
+pub use proto_build as proto_build;
+pub use test_runner as test_runner;
+pub use gravity_proto as gravity_proto;
+pub use client as client;
+pub use register_delegate_keys as register_delegate_keys;
+pub use gorc as gorc;
