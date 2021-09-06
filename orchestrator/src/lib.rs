3167a23f3553f64a28355cf2b36e6b3117cfee6f
hannydevelop <ukpaiugochiibem@gmail.com>
2021-09-06 11:21:57 +0000

    adding relayer to lib

--- orchestrator/src/lib.rs
+++ orchestrator/src/lib.rs
@@ -2,6 +2,6 @@ pub use orchestrator as orchestrator;
 pub use cosmos_gravity as cosmos_gravity;
 pub use ethereum_gravity as ethereum_gravity;
 pub use gravity_utils as gravity_utils;
-pub use test_runner as test_runner;
 pub use gravity_proto as gravity_proto;
 pub use gorc as gorc;
+pub use relayer as relayer;
