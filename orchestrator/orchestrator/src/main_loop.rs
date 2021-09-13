fdf9b3fa935bc8b005638010e145b99f2a87d7cc
hannydevelop <ukpaiugochiibem@gmail.com>
2021-09-13 14:17:32 +0000

    awaiting g

--- orchestrator/orchestrator/src/main_loop.rs
+++ orchestrator/orchestrator/src/main_loop.rs
@@ -87,7 +87,7 @@ pub async fn orchestrator_main_loop(
 
     let f = metrics_main_loop(metrics_listen);
 
-    let g = futures::future::join_all(vec![a, b, c, d, e, f]);
+    let g = futures::future::join_all(vec![a, b, d, e, f]).await;
 }
 
 const DELAY: Duration = Duration::from_secs(5);
