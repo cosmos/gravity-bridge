d4dba29977cf196375e88327bf456718281ad88f
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2021-10-05 17:02:39 -0400

    init commit

--- orchestrator/cosmos_gravity/src/send.rs
+++ orchestrator/cosmos_gravity/src/send.rs
@@ -40,8 +40,8 @@ pub async fn update_gravity_delegate_addresses(
     let nonce = contact
         .get_account_info(cosmos_key.to_address(&contact.get_prefix()).unwrap())
         .await?
-        .get_sequence().unwrap_or( 0 );
-    
+        .get_sequence()
+        .unwrap_or(0);
 
     let eth_sign_msg = proto::DelegateKeysSignMsg {
         validator_address: our_valoper_address.clone(),
@@ -178,9 +178,20 @@ pub async fn send_main_loop(
     mut rx: tokio::sync::mpsc::Receiver<Vec<Msg>>,
 ) {
     while let Some(messages) = rx.recv().await {
-        match send_messages(contact, cosmos_key, gas_price.to_owned(), messages).await {
-            Ok(res) => trace!("okay: {:?}", res),
-            Err(err) => error!("fail: {}", err),
+        // TODO: Consider making the chunk size configurable in the cosmos
+        // configuration.
+        for msg_chunk in messages.chunks(5) {
+            match send_messages(
+                contact,
+                cosmos_key,
+                gas_price.to_owned(),
+                msg_chunk.to_vec(),
+            )
+            .await
+            {
+                Ok(res) => trace!("okay: {:?}", res),
+                Err(err) => error!("fail: {}", err),
+            }
         }
     }
 }
